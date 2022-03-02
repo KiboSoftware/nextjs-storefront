e557ee887122a358f2d2f286b813c9a9d96daac4
kibo-kevinwatts <85258296+kibo-kevinwatts@users.noreply.github.com>
2022-03-02 12:24:58 -0600

    fix(i18n): enable i18n translations on storybook (#11)

---
+++ .storybook/i18n.js
@@ -0,0 +1,18 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18n.use(initReactI18next).init({
+  fallbackLng: 'en',
+  debug: true,
+  react: {
+    wait: true,
+    useSuspense: false,
+  },
+})
+
+const localeAssets = ['common', 'cart-page', 'product-page']
+for (const asset of localeAssets) {
+  i18n.addResourceBundle('en', asset, require(`../public/locales/en/${asset}.json`))
+}
+
+export default i18n
