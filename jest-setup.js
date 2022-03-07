396de622d74c0f403255ae8bfb936fcb2a8d5c7e
Kevin Watts <kevin.watts@kibocommerce.com>
2022-03-07 14:06:26 -0600

    chore: configure storbyook testing react

---
+++ jest-setup.js
@@ -0,0 +1,15 @@
+import { setGlobalConfig } from '@storybook/testing-react'
+import * as globalStorybookConfig from './.storybook/preview'
+
+setGlobalConfig(globalStorybookConfig)
+
+jest.mock('next-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    }
+  },
+}))
