e75806ae7eba0b5ab872c4fa6a3bbd0fb7692741
kibo-sushant <89385472+kibo-sushant@users.noreply.github.com>
2022-03-10 11:02:19 +0530

    feat: Quantity Selector (#14)
    * feat: Quantity Selector initial commit
    
    * feat: Written Test Cases
    
    * feat: cleaned code
    
    * Changed toBeVisible from toBeInTheDocument
    
    * feat: renamed file from .test to .spec
    
    * feat: refactored testcases and used setup() function for common code
    
    * feat: corrected typo QuantitySelecotor => QuantitySelector

---
+++ components/common/QuantitySelector/QuantitySelector.spec.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import QuantitySelector from './QuantitySelector'
+
+describe('[components] - QuantitySelector', () => {
+  const setup = (defaultQuantity = 1) => {
+    const onIncreaseMock = jest.fn()
+    const onDecreaseMock = jest.fn()
+
+    render(
+      <QuantitySelector
+        quantity={defaultQuantity}
+        onIncrease={onIncreaseMock}
+        onDecrease={onDecreaseMock}
+      />
+    )
+
+    return {
+      onIncreaseMock,
+      onDecreaseMock,
+    }
+  }
+
+  it('should render component', () => {
+    // arrange
+    setup()
+
+    // act
+    const input = screen.getByRole('textbox')
+    const decreaseButton = screen.getByRole('button', { name: 'decrease' })
+    const increaseButton = screen.getByRole('button', { name: 'increase' })
+
+    // assert
+    expect(input).toBeVisible()
+    expect(decreaseButton).toBeVisible()
+    expect(increaseButton).toBeVisible()
+  })
+
+  it('should have initial default Quantity = 1', () => {
+    // arrange
+    setup()
+
+    // act
+    const input = screen.getByRole('textbox') as HTMLInputElement
+
+    // assert
+    expect(input).toHaveValue('1')
+  })
+
+  it('should disable decrease button(-) when Quantity = 1 ', () => {
+    // arrange
+    setup()
+
+    // act
+    const input = screen.getByRole('textbox') as HTMLInputElement
+    const decreaseButton = screen.getByRole('button', { name: 'decrease' })
+
+    // assert
+    expect(input).toHaveValue('1')
+    expect(decreaseButton).toHaveAttribute('aria-disabled', 'true')
+  })
+
+  it('should call onIncrease action when user clicks on Increase(+) button', async () => {
+    // arrange
+    const { onIncreaseMock } = setup()
+
+    // act
+    const increaseButton = screen.getByRole('button', { name: 'increase' })
+    userEvent.click(increaseButton)
+
+    // assert
+    expect(onIncreaseMock).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call onDecrease action when user clicks on Decrease(-) button and Quantity > 1', () => {
+    // arrange
+    const defaultQuantity = 2
+    const { onDecreaseMock } = setup(defaultQuantity)
+
+    // act
+    const decreaseButton = screen.getByRole('button', { name: 'decrease' })
+    userEvent.click(decreaseButton)
+
+    // assert
+    expect(onDecreaseMock).toHaveBeenCalledTimes(1)
+  })
+})
---
+++ components/common/QuantitySelector/QuantitySelector.stories.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import QuantitySelector from './QuantitySelector'
+
+// Common
+export default {
+  component: QuantitySelector,
+  title: 'Common/QuantitySelector',
+} as ComponentMeta<typeof QuantitySelector>
+
+const Template: ComponentStory<typeof QuantitySelector> = (args) => {
+  const [quantity, setQuantity] = useState(args.quantity || 1)
+
+  const onDecrease = () => setQuantity((q) => q - 1)
+  const onIncrease = () => setQuantity((q) => q + 1)
+
+  return <QuantitySelector quantity={quantity} onDecrease={onDecrease} onIncrease={onIncrease} />
+}
+
+// Default
+export const Default = Template.bind({})
+Default.args = {
+  quantity: 1,
+}
