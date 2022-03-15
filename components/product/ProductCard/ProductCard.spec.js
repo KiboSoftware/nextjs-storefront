26bdf18add565ff940e40055a4f53af69e878728
Kevin Watts <kevin.watts@kibocommerce.com>
2022-03-15 17:13:09 -0500

    load jest-dom library globally on jest startup to reduce test boiler plate

--- components/product/ProductCard/ProductCard.spec.js
+++ components/product/ProductCard/ProductCard.spec.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
 
 import * as stories from './ProductCard.stories' // import all stories from the stories file
 
