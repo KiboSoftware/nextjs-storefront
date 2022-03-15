481baf3eb61e1c73eba45955e24c2d216890b8bc
Chandradeepta <chandradeepta.laha@kibocommerce.com>
2022-03-15 15:32:40 +0530

    feat(option-list): implementing productOptionList component

---
+++ components/product/ProductOptionList/ProductOptionList.stories.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import ProductOptionList from './ProductOptionList'
+import { ProductOption } from '@/lib/gql/types'
+
+const values: ProductOption[] = [
+  {
+    attributeFQN: 'Tenant~optional-mount',
+    attributeDetail: {
+      dataTypeSequence: 13,
+      name: 'Optional Mount',
+    },
+    values: [
+      {
+        value: 'MS-CAM-005',
+        attributeValueId: 125,
+        stringValue: '125',
+      },
+      {
+        value: 'MS-CAM-006',
+        attributeValueId: 126,
+        stringValue: '126',
+      },
+      {
+        value: 'MS-CAM-007',
+        attributeValueId: 127,
+        stringValue: '127',
+      },
+    ],
+  },
+  {
+    attributeFQN: 'Tenant~size',
+    attributeDetail: {
+      dataTypeSequence: 13,
+      name: 'Size',
+    },
+    values: [
+      {
+        value: 'L',
+        attributeValueId: 125,
+        stringValue: 'Large',
+      },
+      {
+        value: 'M',
+        attributeValueId: 125,
+        stringValue: 'Medium',
+      },
+      {
+        value: 'S',
+        attributeValueId: 125,
+        stringValue: 'Small',
+      },
+    ],
+  },
+]
+
+export default {
+  title: 'Product/Product Option List',
+  component: ProductOptionList,
+  argTypes: { handleChange: { action: 'handleChange' } },
+} as ComponentMeta<typeof ProductOptionList>
+
+const Template: ComponentStory<typeof ProductOptionList> = (args) => <ProductOptionList {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  listOptions: values,
+  error: false,
+  errorHelperText: 'value not valid',
+  value: '',
+}
---
+++ components/product/ProductOptionList/ProductOptionList.tsx
@@ -0,0 +1,47 @@
+import { MenuItem } from '@mui/material'
+import { Box } from '@mui/system'
+
+import KiboSelect from '@/components/common/KiboSelect/KiboSelect'
+import { ProductOption } from '@/lib/gql/types'
+
+interface ProductOptionListProps {
+  listOptions: ProductOption[]
+  error?: boolean
+  errorHelperText?: string
+  value: string
+  row?: boolean
+  placeholder?: string
+  handleChange: (value: string) => void
+}
+
+export default function ProductOptionList({
+  listOptions,
+  error = false,
+  errorHelperText = '',
+  value,
+  row,
+  handleChange,
+}: ProductOptionListProps) {
+  return (
+    <Box display={'flex'} flexDirection={row ? 'row' : 'column'} gap={1}>
+      {listOptions.map((option: ProductOption) => {
+        return (
+          <KiboSelect
+            key={option?.attributeDetail?.name}
+            error={error}
+            errorHelperText={errorHelperText}
+            handleChange={handleChange}
+            value={value}
+            placeholder="Select product option"
+          >
+            {option.values?.map((optionVal) => (
+              <MenuItem key={optionVal?.value} value={optionVal?.value}>
+                {optionVal?.stringValue}
+              </MenuItem>
+            ))}
+          </KiboSelect>
+        )
+      })}
+    </Box>
+  )
+}
