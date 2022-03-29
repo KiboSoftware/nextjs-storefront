92ed24514aa4cd1607ebcc6e8b794623c1509160
Susanta Samanta <Susanta.Samanta@blueconchtech.com>
2022-03-29 20:12:07 +0530

    fix lint issue

--- components/product/ColorSelector/ColorSelector.stories.tsx
+++ components/product/ColorSelector/ColorSelector.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import ColorSelector from './ColorSelector'
+
 import { ProductOptionValue } from '@/lib/gql/types'
 const values: ProductOptionValue[] = [
   {
