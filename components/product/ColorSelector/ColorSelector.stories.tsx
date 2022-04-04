9d17de2c323dd4416a25a066e90c11f91411e9a5
sss-bct <92077348+sss-bct@users.noreply.github.com>
2022-04-04 22:29:46 +0530

    feat(colorVariant): implement product color variant (#21)
    * feat(colorVariant) : implement product color variant
    
    * resolve review comments
    
    * fix to align with product option
    
    * minor css changes
    
    * css fix
    
    * fix lint issue
    
    * fix code smell
    
    * resolve PR review changes
    
    * refine with code standardization

---
+++ components/product/ColorSelector/ColorSelector.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import ColorSelector from './ColorSelector'
+
+import { ProductOptionValue } from '@/lib/gql/types'
+const values: ProductOptionValue[] = [
+  {
+    attributeValueId: 1,
+    value: 'violet',
+    isSelected: true,
+  },
+  {
+    attributeValueId: 2,
+    value: 'ingido',
+    isEnabled: false,
+  },
+  {
+    attributeValueId: 3,
+    value: 'blue',
+  },
+  { attributeValueId: 4, value: 'green' },
+  { attributeValueId: 5, value: 'yellow', isEnabled: false },
+  { attributeValueId: 6, value: 'orange', isSelected: false },
+  { attributeValueId: 7, value: 'red', isSelected: false, isEnabled: true },
+  { attributeValueId: 8, value: 'white', isSelected: false, isEnabled: true },
+]
+
+export default {
+  title: 'Product/ColorSelector',
+  component: ColorSelector,
+  argTypes: { onChange: { action: 'onChange' } },
+} as ComponentMeta<typeof ColorSelector>
+
+const Template: ComponentStory<typeof ColorSelector> = (args) => <ColorSelector {...args} />
+
+export const Common = Template.bind({})
+Common.args = {
+  attributeFQN: 'tenant~color',
+  values,
+}
---
+++ components/product/ColorSelector/ColorSelector.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+import { Box, Chip, Tooltip } from '@mui/material'
+import { grey } from '@mui/material/colors'
+import { useTranslation } from 'next-i18next'
+
+import { ProductOptionValue } from '@/lib/gql/types'
+
+interface ColorSelectorProps {
+  attributeFQN: string
+  values: ProductOptionValue[]
+  onChange: (attributeFQN?: string, value?: string) => void
+}
+
+interface ColorOptionsProps extends ProductOptionValue {
+  attributeFQN?: string
+  onColorSelection: (attributeFQN?: string, value?: string) => void
+}
+
+const styles = {
+  productVariantColor: {
+    width: '1.938rem',
+    height: '1.938rem',
+    borderRadius: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+    elevation: 0,
+    borderColor: `${grey[300]}`,
+    borderWidth: '1px',
+    borderStyle: 'solid',
+  },
+  selected: {
+    outlineColor: 'common.black',
+    outlineWidth: '1px',
+    outlineStyle: 'solid',
+    outlineOffset: 4,
+    elevation: 2,
+    cursor: 'default',
+  },
+  disabled: {
+    cursor: 'default',
+  },
+}
+
+const ColorOptions = (props: ColorOptionsProps) => {
+  const { attributeFQN, value, isSelected = false, isEnabled = true, onColorSelection } = props
+  const { t } = useTranslation('common')
+  const chipCustom = (
+    <Chip
+      sx={{
+        ...styles.productVariantColor,
+        ...(isSelected && styles.selected),
+        ...(!isEnabled && styles.disabled),
+        backgroundColor: `${value}`,
+      }}
+      {...(!isSelected && isEnabled && { onClick: () => onColorSelection(attributeFQN, value) })}
+      data-testid={`colorvalue-${value}`}
+    ></Chip>
+  )
+
+  return isEnabled ? (
+    <>{chipCustom}</>
+  ) : (
+    <Tooltip title={t('not-available !') || ''}>{chipCustom}</Tooltip>
+  )
+}
+
+const ColorSelector = ({ attributeFQN, values, onChange }: ColorSelectorProps) => {
+  return (
+    <Box display="flex" flexWrap="wrap" gap={2} data-testid="color-selector">
+      {values?.map((option) => (
+        <ColorOptions
+          key={option.attributeValueId}
+          attributeValueId={option.attributeValueId}
+          attributeFQN={attributeFQN}
+          value={option?.value}
+          isSelected={option?.isSelected}
+          isEnabled={option?.isEnabled}
+          onColorSelection={onChange}
+        />
+      ))}
+    </Box>
+  )
+}
+
+export default ColorSelector
