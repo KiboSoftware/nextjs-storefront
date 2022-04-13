501bc9c45151d63a64bfbcec19298dce3b6e6660
kibo-Kripa <kripa.venkateswaran@kibocommerce.com>
2022-04-13 11:17:41 -0500

    Styles fix

--- components/common/OrderSummary/OrderSummary.tsx
+++ components/common/OrderSummary/OrderSummary.tsx
@@ -1,34 +1,70 @@
 /** @format */
+import InfoIcon from '@mui/icons-material/Info'
+import { Card, Typography, Box, Button, CardActionArea, CardContent, Divider } from '@mui/material'
 
-import {
-  Card,
-  Typography,
-  Box,
-  Button,
-  CardActionArea,
-  CardContent,
-  Divider,
-} from "@mui/material";
-import InfoIcon from "@mui/icons-material/Info";
 interface OrderTotalProps {
-  subTotal?: string;
-  standardShipping?: string;
-  estTax?: string;
-  orderTotal?: string;
-  name: string;
-  cartTotal: string;
-  fontWeight?: "bold" | "regular";
-  boxStyle: Object;
-  style2: Object;
-  standardShippingAmount: string;
-  estTaxamt: string;
-  estOrderTotal: string;
-  checkOutButtonStyle: object;
-  headerStyle: object;
-  estStyle: object;
-  backButtonStyle: object;
-  shippingButtonStyle: object;
-  type: string;
+  subTotal?: string
+  standardShipping?: string
+  estTax?: string
+  orderTotal?: string
+  name: string
+  cartTotal: string
+  fontWeight?: 'bold' | 'regular'
+  boxStyle: any
+  style2: any
+  standardShippingAmount: string
+  estTaxamt: string
+  estOrderTotal: string
+  checkOutButtonStyle: any
+  headerStyle: any
+  estStyle: any
+  backButtonStyle: any
+  shippingButtonStyle: any
+  type: string
+}
+
+const styles = {
+  boxStyle: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: '14px',
+    lineHeight: '17px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  estStyle: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: '14px',
+    lineHeight: '17px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    fontWeight: 'bold',
+  },
+  headerStyle: {
+    color: ' #2B2B2B',
+    fontSize: '20px',
+    lineHeight: '24px',
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
+  checkOutButtonStyle: {
+    borderradius: '4px',
+    width: '376px',
+    height: '42px',
+  },
+  shippingButtonStyle: {
+    // backgroundcolor: "#2EA195",
+    borderradius: '4px',
+    margin: '10px',
+    width: '376px',
+    height: '42px',
+  },
+  backButtonStyle: {
+    color: 'black',
+    backgroundColor: 'white',
+    borderradius: '4px',
+    width: '376px',
+    height: '42px',
+  },
 }
 
 const OrderSummary = (props: OrderTotalProps) => {
@@ -39,39 +75,33 @@ const OrderSummary = (props: OrderTotalProps) => {
     estTax,
     orderTotal,
     name,
-    boxStyle,
     standardShippingAmount,
     estTaxamt,
     estOrderTotal,
-    checkOutButtonStyle,
-    headerStyle,
-    estStyle,
-    shippingButtonStyle,
-    backButtonStyle,
     type,
-  } = props;
+  } = props
   return (
     <>
-      <Card sx={{ bgcolor: "#F7F7F7", width: "428px", height: "391px" }}>
+      <Card sx={{ bgcolor: '#F7F7F7', width: '428px', height: '391px' }}>
         <CardActionArea>
           <CardContent>
-            <Typography sx={headerStyle}>{name}</Typography>
+            <Typography sx={styles.headerStyle}>{name}</Typography>
           </CardContent>
           <Divider />
           <CardContent>
-            <Box sx={boxStyle}>
+            <Box sx={styles.boxStyle}>
               <Typography>{cartTotal}</Typography>
               <Typography> {subTotal}</Typography>
             </Box>
             <br />
-            <Box sx={boxStyle}>
+            <Box sx={styles.boxStyle}>
               <Typography>{standardShipping}</Typography>
               <Typography>{standardShippingAmount}</Typography>
             </Box>
             <br />
-            <Box sx={boxStyle}>
+            <Box sx={styles.boxStyle}>
               <Typography>
-                {estTax} <InfoIcon sx={{ width: "11px", height: "11px" }} />{" "}
+                {estTax} <InfoIcon sx={{ width: '11px', height: '11px' }} />{' '}
               </Typography>
               <Typography>{estTaxamt}</Typography>
             </Box>
@@ -79,28 +109,26 @@ const OrderSummary = (props: OrderTotalProps) => {
             <Divider variant="middle" />
 
             <br />
-            <Box sx={estStyle}>
-              <Typography sx={{ fontWeight: "Bold" }}>
-                {estOrderTotal}
-              </Typography>
-              <Typography sx={{ fontWeight: "Bold" }}>{orderTotal}</Typography>
+            <Box sx={styles.estStyle}>
+              <Typography sx={{ fontWeight: 'Bold' }}>{estOrderTotal}</Typography>
+              <Typography sx={{ fontWeight: 'Bold' }}>{orderTotal}</Typography>
             </Box>
           </CardContent>
           <CardContent>
             <Box textAlign="center">
-              {type === "orderSummary" && (
+              {type === 'orderSummary' && (
                 <>
-                  <Button variant="contained" sx={checkOutButtonStyle}>
+                  <Button variant="contained" sx={styles.checkOutButtonStyle}>
                     Go to Checkout
                   </Button>
                 </>
               )}
-              {type === "orderShipping" && (
+              {type === 'orderShipping' && (
                 <>
-                  <Button variant="contained" sx={shippingButtonStyle} disabled>
+                  <Button variant="contained" sx={styles.shippingButtonStyle} disabled>
                     Go to Shipping
                   </Button>
-                  <Button variant="contained" sx={backButtonStyle}>
+                  <Button variant="contained" sx={styles.backButtonStyle}>
                     Go Back
                   </Button>
                 </>
@@ -110,6 +138,6 @@ const OrderSummary = (props: OrderTotalProps) => {
         </CardActionArea>
       </Card>
     </>
-  );
-};
-export default OrderSummary;
+  )
+}
+export default OrderSummary
