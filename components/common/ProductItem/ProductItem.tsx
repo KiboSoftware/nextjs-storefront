1dd0f411d4378f6f166eaf0ed5ff6f783227bebe
Susanta Samanta <Susanta.Samanta@blueconchtech.com>
2022-05-09 13:10:19 +0530

    resolve code smell

--- components/common/ProductItem/ProductItem.tsx
+++ components/common/ProductItem/ProductItem.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, SyntheticEvent, useState } from 'react'
+import React, { ReactNode, useState } from 'react'
 
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
--- components/core/ImageGallery/ImageGallery.tsx
+++ components/core/ImageGallery/ImageGallery.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, SyntheticEvent } from 'react'
+import { useState, useRef } from 'react'
 
 import {
   ArrowBackIos,
