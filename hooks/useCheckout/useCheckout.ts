5c6b1f91f1d59c4130e32f689f0cbad663650cc1
kibo-kevinwatts <85258296+kibo-kevinwatts@users.noreply.github.com>
2022-05-18 23:13:32 -0500

    feat: add create graphql client util method (#47)

--- hooks/useCheckout/useCheckout.ts
+++ hooks/useCheckout/useCheckout.ts
@@ -1,7 +1,8 @@
 import { request } from 'graphql-request'
 import { useQuery } from 'react-query'
 
-import { querykeys } from '../../lib/react-query/queryKeys'
+import { makeGraphQLClient } from '@/lib/gql/client'
+import { querykeys } from '@/lib/react-query/queryKeys'
 import { getCheckoutQuery, getOrCreateCheckoutFromCartMutation } from '@/lib/gql/queries'
 
 import type { Order } from '@/lib/gql/types'
@@ -16,8 +17,8 @@ export interface UseCheckoutResponse {
 }
 
 const getOrCreateCheckout = async (cartId?: string | null, checkoutId?: string | null) => {
-  const response = await request({
-    url: '/api/graphql',
+  const client = makeGraphQLClient()
+  const response = await client.request({
     document: checkoutId ? getCheckoutQuery : getOrCreateCheckoutFromCartMutation,
     variables: checkoutId ? { checkoutId } : { cartId },
   })
