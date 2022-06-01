2b8951b7ae30c40d6fbef93bddd4d28a2f3ddb8a
amolpawar-kibo <amol.pawar@kibocommerce.com>
2022-06-01 19:57:40 +0530

    feat(PaymentStep): add addressform tests

--- components/common/AddressForm/AddressForm.spec.tsx
+++ components/common/AddressForm/AddressForm.spec.tsx
@@ -93,4 +93,13 @@ describe('[components] - AddressForm', () => {
 
     expect(onHandleSubmitMock).toHaveBeenCalled()
   })
+  it('should not render when isUserLoggedIn = false', () => {
+    // arrange
+    render(<Common {...Common.args} isUserLoggedIn={false} />)
+    // act
+    const saveShippingAddress = screen.queryByRole('checkbox')
+
+    // asert
+    expect(saveShippingAddress).not.toBeInTheDocument()
+  })
 })
