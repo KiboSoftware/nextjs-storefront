2a32979a4ab6594e861ae799fa8853d1b5f3992e
TriptiVerma <tripti.verma@kibocommerce.com>
2022-06-07 19:36:15 +0530

    ICKY-612 Updated lock file

--- hooks/custom/useDebounce/useDebounce.ts
+++ hooks/custom/useDebounce/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 export const useDebounce = (value: string, delay: number) => {
-  const [debouncedValue, setDebouncedValue] = useState(value)
+  const [debouncedValue, setDebouncedValue] = useState<string>(value)
 
   useEffect(() => {
     const handler = setTimeout(() => {
