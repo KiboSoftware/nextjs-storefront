c83c7ea5bb8654a7b195771262d09c106551445d
TriptiVerma <tripti.verma@kibocommerce.com>
2022-06-14 16:46:35 +0530

    ICKY-612 Resolved review comments

--- components/layout/HamburgerMenu/HamburgerMenu.tsx
+++ components/layout/HamburgerMenu/HamburgerMenu.tsx
@@ -8,29 +8,52 @@ import { useRouter } from 'next/router'
 
 import HeaderAction from '@/components/common/HeaderAction/HeaderAction'
 import { CategoryNestedNavigation } from '@/components/layout'
+import type { NavigationLink } from '@/lib/types'
 
 import type { Maybe, PrCategory } from '@/lib/gql/types'
 
-interface NavLinkProps {
-  text: string
-  link: string
-}
 interface HamburgerMenuProps {
   categoryTree: Maybe<PrCategory>[]
   isDrawerOpen: boolean
-  navLinks?: NavLinkProps[]
+  navLinks?: NavigationLink[]
   marginTop?: number | string
   setIsDrawerOpen: (isDrawerOpen: boolean) => void
 }
 
+const styles = {
+  container: {
+    width: '80vw',
+    position: 'relative',
+    height: '-webkit-fill-available',
+    overflowY: 'hidden',
+    display: 'flex',
+    flexDirection: 'column',
+    maxHeight: '60%',
+  },
+  menuList: {
+    overflowY: 'auto',
+    width: '100%',
+    flex: 1,
+    pt: 2,
+  },
+  spacer: {
+    backgroundColor: 'grey.300',
+    height: 19,
+  },
+  navLinksList: {
+    width: '100%',
+    maxHeight: '40%',
+    overflowY: 'auto',
+    pt: 0,
+  },
+}
+
 const HamburgerMenu = (props: HamburgerMenuProps) => {
   const { categoryTree, isDrawerOpen, marginTop = 7, setIsDrawerOpen, navLinks } = props
-  const [isOpen, setIsOpen] = useState(false)
   const { t } = useTranslation('common')
   const router = useRouter()
 
   const toggleDrawer = (open: boolean) => {
-    setIsOpen(open)
     setIsDrawerOpen(open)
   }
 
@@ -39,31 +62,16 @@ const HamburgerMenu = (props: HamburgerMenuProps) => {
     router.push('/product/' + categoryCode)
   }
 
-  useEffect(() => {
-    setIsOpen(isDrawerOpen)
-  }, [isDrawerOpen])
-
   return (
     <SwipeableDrawer
       anchor={'left'}
-      open={isOpen}
+      open={isDrawerOpen}
       onClose={() => toggleDrawer(false)}
       onOpen={() => toggleDrawer(true)}
       data-testid="hamburger-menu"
     >
-      <Box
-        sx={{
-          width: '80vw',
-          marginTop: marginTop,
-          position: 'relative',
-          height: '-webkit-fill-available',
-          overflowY: 'hidden',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-        role="presentation"
-      >
-        <Box maxHeight={'60%'} sx={{ overflowY: 'auto', width: '100%', flex: 1, pt: 2 }}>
+      <Box sx={{ ...styles.container, marginTop: marginTop }} role="presentation">
+        <Box sx={{ ...styles.menuList }}>
           <CategoryNestedNavigation
             categoryTree={categoryTree}
             onCloseMenu={toggleDrawer}
@@ -81,8 +89,8 @@ const HamburgerMenu = (props: HamburgerMenuProps) => {
             </Box>
           </CategoryNestedNavigation>
         </Box>
-        <Box height={19} sx={{ backgroundColor: 'grey.300' }}></Box>
-        <List sx={{ width: '100%', maxHeight: '40%', overflowY: 'auto', pt: 0 }}>
+        <Box sx={{ ...styles.spacer }}></Box>
+        <List sx={{ ...styles.navLinksList }}>
           {navLinks?.map((nav) => (
             <Box key={nav.text}>
               <Link href={nav.link} passHref>
