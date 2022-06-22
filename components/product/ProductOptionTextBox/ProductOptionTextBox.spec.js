b67f4fe2843196278688dc41e0c65c9c2a773ec8
Chandradeepta <chandradeepta.laha@kibocommerce.com>
2022-06-22 16:41:34 +0530

    fix: unit test error

--- components/product/ProductOptionTextBox/ProductOptionTextBox.spec.js
+++ components/product/ProductOptionTextBox/ProductOptionTextBox.spec.js
@@ -6,28 +6,29 @@ import userEvent from '@testing-library/user-event'
 import * as stories from './ProductOptionTextBox.stories' // import all stories from the stories file
 
 const { Common } = composeStories(stories)
+const mockedOnBlur = jest.fn()
 
 const setup = () => {
-  const onBlurMock = jest.fn()
-  render(<Common {...Common.args} onBlur={onBlurMock} />)
-  return onBlurMock
+  render(<Common {...Common.args} onBlur={mockedOnBlur} />)
 }
 
 describe('[component] ProductOptionTextBox component', () => {
   it('should render option', () => {
     setup()
-    const textbox = screen.getByTestId('kibo-textbox-component')
+
+    const textbox = screen.getByRole('textbox')
+
     expect(textbox).toBeInTheDocument()
   })
 
   it('should show user entered value', () => {
-    const { onBlurMock } = setup()
+    setup()
 
     const textbox = screen.getByRole('textbox')
     userEvent.type(textbox, 'Test')
     fireEvent.blur(textbox)
 
     expect(textbox).toHaveValue('Test')
-    expect(onBlurMock).toHaveBeenCalled()
+    expect(mockedOnBlur).toHaveBeenCalled()
   })
 })
