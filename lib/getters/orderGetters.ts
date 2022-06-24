f5943cf8e1ffb6312259b1275e38c936505bbf68
TriptiVerma <tripti.verma@kibocommerce.com>
2022-06-24 15:09:15 +0530

    ICKY-627 Resolved quality bug issue

--- lib/getters/orderGetters.ts
+++ lib/getters/orderGetters.ts
@@ -13,7 +13,11 @@ const getSubmittedDate = (order: Order, withTimestamp?: boolean) => {
       ? format(new Date(order?.submittedDate), 'MMMM D, YYYY, hh:mm a zzz')
       : format(new Date(order?.submittedDate), 'MMMM D, YYYY')
   }
-  order?.submittedDate
+  return order?.submittedDate
+    ? withTimestamp
+      ? format(new Date(order?.submittedDate), 'MMMM D, YYYY, hh:mm a zzz')
+      : format(new Date(order?.submittedDate), 'MMMM D, YYYY')
+    : order?.submittedDate
 }
 
 const getExpectedDeliveryDate = (items: Maybe<CrOrderItem>[]) => {
