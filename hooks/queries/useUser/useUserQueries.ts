292a8397006612a655709f2c3256599d330a5297
Susanta Samanta <susanta.samanta@blueconchtech.com>
2022-06-24 17:00:28 +0530

    add type for useUserQueries return type

--- hooks/queries/useUser/useUserQueries.ts
+++ hooks/queries/useUser/useUserQueries.ts
@@ -4,6 +4,14 @@ import { makeGraphQLClient } from '@/lib/gql/client'
 import { getCurrentUser } from '@/lib/gql/mutations/user/getUser'
 import { loginKeys } from '@/lib/react-query/queryKeys'
 
+import type { CustomerAccount } from '@/lib/gql/types'
+export interface UserResultType {
+  data: CustomerAccount
+  isLoading: boolean
+  isSuccess: boolean
+  isError: boolean
+  error: any
+}
 const client = makeGraphQLClient()
 
 const loadUser = async () => {
@@ -15,7 +23,7 @@ const loadUser = async () => {
   return response?.customerAccount
 }
 
-export const useUserQueries = () => {
+export const useUserQueries = (): UserResultType => {
   const { data, isLoading, isSuccess, isError, error } = useQuery(loginKeys.user, loadUser)
   return {
     data,
