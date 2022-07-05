addca09c6e3d8353eae2503b752adbda98e7d1ff
TriptiVerma <tripti.verma@kibocommerce.com>
2022-07-05 15:06:52 +0530

    ICKY-624 Implemented store locator UI

---
+++ components/store/StoreDetails/StoreDetails.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import StoreDetails from './StoreDetails'
+import { locationCollectionMock } from '@/__mocks__/stories/locationCollectionMock'
+import { storeLocationGetters } from '@/lib/getters/storeLocationGetters'
+
+export default {
+  title: 'store/StoreDetails',
+  component: StoreDetails,
+} as ComponentMeta<typeof StoreDetails>
+
+const Template: ComponentStory<typeof StoreDetails> = ({ ...args }) => <StoreDetails {...args} />
+
+// Common
+export const Common = Template.bind({})
+
+Common.args = storeLocationGetters.getLocations(locationCollectionMock?.spLocations?.items || [])[0]
