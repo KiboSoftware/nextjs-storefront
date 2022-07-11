98058b63923f0b96695795f9dbdc21f762e929cf
TriptiVerma <tripti.verma@kibocommerce.com>
2022-07-11 18:23:48 +0530

    ICKY-624 Handled stories

--- components/dialogs/Store/MyStoreDialog/MyStoreDialog.spec.tsx
+++ components/dialogs/Store/MyStoreDialog/MyStoreDialog.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import * as stories from './MyStoreDialog.stories'
 import { ModalContextProvider } from '@/context'
