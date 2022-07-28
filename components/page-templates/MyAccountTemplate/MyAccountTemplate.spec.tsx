067d08a4a2daf4550929bb637ef6f078947c77d1
kibo-geetanshuchhabra <geetanshu.chhabra@kibocommerce.com>
2022-07-28 12:48:16 +0530

    add test case for usemediaquery

--- components/page-templates/MyAccountTemplate/MyAccountTemplate.spec.tsx
+++ components/page-templates/MyAccountTemplate/MyAccountTemplate.spec.tsx
@@ -5,17 +5,31 @@ import '@testing-library/jest-dom'
 import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import mediaQuery from 'css-mediaquery'
 
 import * as stories from './MyAccountTemplate.stories' // import all stories from the stories file
 const { Common } = composeStories(stories)
 
+const createMatchMedia = (width: number) => (query: string) => ({
+  matches: mediaQuery.match(query, { width }),
+  addListener: () => jest.fn(),
+  removeListener: () => jest.fn(),
+  media: query,
+  onchange: null,
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+})
+
+const FullWidthDividerMock = () => <div data-testid="full-width-divider-component" />
+jest.mock('../../common/FullWidthDivider/FullWidthDivider', () => FullWidthDividerMock)
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 const push = jest.fn()
 useRouter.mockImplementation(() => ({
   push,
 }))
 
-describe('[component] - Category', () => {
+describe('[component] - MyAccountTemplate', () => {
   const setup = () => {
     const user = userEvent.setup()
     render(<Common />)
@@ -52,5 +66,27 @@ describe('[component] - Category', () => {
     await user.click(orderHistory)
 
     expect(push).toHaveBeenCalledWith('/my-account/order-history')
+  })
+
+  it('Desktop', async () => {
+    window.matchMedia = createMatchMedia(1000)
+
+    setup()
+    const fullWidthDividerComponent = screen.queryByTestId('full-width-divider-component')
+    const headingArray = screen.getAllByRole('heading', { level: 1 })
+    expect(fullWidthDividerComponent).not.toBeInTheDocument()
+    expect(headingArray[0]).toHaveTextContent('common:my-account')
+    expect(headingArray[1]).toHaveTextContent('order-details')
+  })
+
+  it('Mobile', async () => {
+    window.matchMedia = createMatchMedia(200)
+
+    setup()
+    const fullWidthDividerComponent = screen.queryAllByTestId('full-width-divider-component')
+    const headingArray = screen.getAllByRole('heading', { level: 2 })
+    expect(fullWidthDividerComponent.length).toEqual(7)
+    expect(headingArray[0]).toHaveTextContent('common:my-account')
+    expect(headingArray[1]).toHaveTextContent('order-details')
   })
 })
