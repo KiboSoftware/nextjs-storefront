94fee6fd7bb13a2c6b646175c0b517556a759181
kibo-sushant <sushant.jadhav@blueconchtech.com>
2022-07-29 14:34:40 +0530

    feat: Order Review Step

--- components/checkout/DetailsStep/DetailsStep.tsx
+++ components/checkout/DetailsStep/DetailsStep.tsx
@@ -21,20 +21,14 @@ import { useForm, Controller } from 'react-hook-form'
 import * as yup from 'yup'
 
 import KiboTextBox from '@/components/common/KiboTextBox/KiboTextBox'
-import PasswordValidation from '@/components/common/PasswordValidation/PasswordValidation'
 import { LoginDialog } from '@/components/layout'
 import { useAuthContext, useCheckoutStepContext, STEP_STATUS, useModalContext } from '@/context'
 import { PersonalInfo, useUpdateCheckoutPersonalInfo } from '@/hooks'
 import { FormStates } from '@/lib/constants'
-import { isPasswordValid } from '@/lib/helpers/validations/validations'
 
 import type { Order, OrderInput, Maybe } from '@/lib/gql/types'
 export interface PersonalDetails {
   email: Maybe<string> | undefined
-  showAccountFields: boolean
-  firstName: string
-  lastNameOrSurname: string
-  password: string
 }
 
 export interface Action {
@@ -92,14 +86,8 @@ const DetailsStep = (props: DetailsProps) => {
     setStepStatusIncomplete,
   } = useCheckoutStepContext()
 
-  const fulfillmentInfo = checkout?.fulfillmentInfo
-  const fulfillmentContact = fulfillmentInfo && fulfillmentInfo?.fulfillmentContact
   const personalDetails = {
     email: checkout && checkout.email,
-    showAccountFields: false,
-    firstName: (fulfillmentContact && fulfillmentContact.firstName) || '',
-    lastNameOrSurname: (fulfillmentContact && fulfillmentContact.lastNameOrSurname) || '',
-    password: '',
   }
 
   const openLoginModal = () => {
@@ -111,8 +99,6 @@ const DetailsStep = (props: DetailsProps) => {
     formState: { errors, isValid },
     handleSubmit,
     control,
-    watch,
-    getValues,
     reset,
   } = useForm({
     mode: 'onBlur',
@@ -121,16 +107,6 @@ const DetailsStep = (props: DetailsProps) => {
     resolver: yupResolver(useDetailsSchema()),
     shouldFocusError: true,
   })
-
-  const showAccountFields = watch(['showAccountFields']).join('')
-  const userEnteredPassword = watch(['password']).join('')
-  const isUserEnteredPasswordValid = () => {
-    return showAccountFields === 'true' ? isPasswordValid(userEnteredPassword) : true
-  }
-
-  const createAccount = async (formData: PersonalDetails) => {
-    console.log(`createAccount: ${JSON.stringify(formData)}`)
-  }
 
   const updatePersonalInfo = async (formData: PersonalDetails) => {
     const { email } = formData
@@ -146,19 +122,9 @@ const DetailsStep = (props: DetailsProps) => {
     await updateCheckoutPersonalInfo.mutateAsync(personalInfo)
   }
 
-  // if form is valid, onSubmit callback
   const onValid = async (formData: PersonalDetails) => {
     try {
-      if (!isUserEnteredPasswordValid()) {
-        setStepStatusIncomplete()
-        return
-      }
-
       await updatePersonalInfo(formData)
-      if (formData?.showAccountFields) {
-        await createAccount(formData)
-      }
-
       setStepStatusComplete()
       setStepNext()
     } catch (error) {
@@ -167,7 +133,6 @@ const DetailsStep = (props: DetailsProps) => {
     }
   }
 
-  // form is invalid, notify parent form is incomplete
   const onInvalidForm = (_errors?: any, _e?: any) => {
     setStepStatusIncomplete()
   }
@@ -207,6 +172,7 @@ const DetailsStep = (props: DetailsProps) => {
       <Typography variant="h2" component="h2" sx={{ fontWeight: 'bold' }}>
         {t('personal-details')}
       </Typography>
+
       <Box>
         <Controller
           name="email"
@@ -228,117 +194,37 @@ const DetailsStep = (props: DetailsProps) => {
           )}
         />
       </Box>
-      <Box>{t('enjoy-these-perks-with-your-free-account')}</Box>
-      <Grid container>
-        <Grid item xs={12} md={4}>
-          <IconButton aria-label={t('faster-checkout')}>
-            <AccessTime fontSize="medium" />
-          </IconButton>
-          {t('faster-checkout')}
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <IconButton aria-label={t('earn-credits-with-every-purchase')}>
-            <EmojiEvents fontSize="medium" />
-          </IconButton>
-          {t('earn-credits-with-every-purchase')}
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <IconButton aria-label={t('full-rewards-program-benifits')}>
-            <CardGiftcard fontSize="medium" />
-          </IconButton>
-          {t('full-rewards-program-benifits')}
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <IconButton aria-label={t('manage-your-wishlist')}>
-            <FavoriteBorder fontSize="medium" />
-          </IconButton>
-          {t('manage-your-wishlist')}
-        </Grid>
-      </Grid>
-      <Box>
-        <FormControl>
-          <Controller
-            name="showAccountFields"
-            control={control}
-            defaultValue={personalDetails?.showAccountFields}
-            render={({ field }) => (
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    inputProps={{
-                      'aria-label': 'showAccountFields',
-                    }}
-                    data-testid="showAccountFields"
-                    size="medium"
-                    color="primary"
-                    onChange={(_name, value) => field.onChange(value)}
-                  />
-                }
-                label={t('i-want-to-create-an-account').toString()}
-              />
-            )}
-          />
-        </FormControl>
-      </Box>
-      {getValues()?.showAccountFields && (
-        <FormControl>
-          <Controller
-            name="firstName"
-            control={control}
-            defaultValue={personalDetails?.firstName}
-            render={({ field }) => (
-              <KiboTextBox
-                value={field.value}
-                label={t('first-name')}
-                required
-                sx={{ ...commonStyle }}
-                onBlur={field.onBlur}
-                onChange={(_name, value) => field.onChange(value)}
-                error={!!errors?.firstName}
-                helperText={errors?.firstName?.message}
-              />
-            )}
-          />
-          <Controller
-            name="lastNameOrSurname"
-            control={control}
-            defaultValue={personalDetails?.lastNameOrSurname}
-            render={({ field }) => (
-              <KiboTextBox
-                value={field.value}
-                label={t('last-name-or-sur-name')}
-                required
-                sx={{ ...commonStyle }}
-                onBlur={field.onBlur}
-                onChange={(_name, value) => field.onChange(value)}
-                error={!!errors?.lastNameOrSurname}
-                helperText={errors?.lastNameOrSurname?.message}
-              />
-            )}
-          />
-          <Controller
-            name="password"
-            control={control}
-            defaultValue={personalDetails?.password}
-            render={({ field }) => (
-              <KiboTextBox
-                value={field.value}
-                label={t('password')}
-                required
-                sx={{ ...commonStyle }}
-                onBlur={field.onBlur}
-                onChange={(_name, value) => field.onChange(value)}
-                error={!!errors?.password}
-                helperText={errors?.password?.message}
-                type="password"
-                placeholder="password"
-              />
-            )}
-          />
 
-          <PasswordValidation password={userEnteredPassword} />
-        </FormControl>
-      )}
+      <Stack gap={2}>
+        {t('enjoy-these-perks-with-your-free-account')}
+
+        <Grid container>
+          <Grid item xs={12}>
+            <IconButton aria-label={t('faster-checkout')}>
+              <AccessTime fontSize="medium" />
+            </IconButton>
+            {t('faster-checkout')}
+          </Grid>
+          <Grid item xs={12}>
+            <IconButton aria-label={t('earn-credits-with-every-purchase')}>
+              <EmojiEvents fontSize="medium" />
+            </IconButton>
+            {t('earn-credits-with-every-purchase')}
+          </Grid>
+          <Grid item xs={12}>
+            <IconButton aria-label={t('full-rewards-program-benifits')}>
+              <CardGiftcard fontSize="medium" />
+            </IconButton>
+            {t('full-rewards-program-benifits')}
+          </Grid>
+          <Grid item xs={12}>
+            <IconButton aria-label={t('manage-your-wishlist')}>
+              <FavoriteBorder fontSize="medium" />
+            </IconButton>
+            {t('manage-your-wishlist')}
+          </Grid>
+        </Grid>
+      </Stack>
     </Stack>
   )
 }
--- components/checkout/OrderReview/OrderReview.tsx
+++ components/checkout/OrderReview/OrderReview.tsx
@@ -1,9 +1,21 @@
 import React, { MouseEvent } from 'react'
 
-import { Typography, Box, Divider, Link, styled, Theme } from '@mui/material'
+import { ExpandMore } from '@mui/icons-material'
+import {
+  Typography,
+  Box,
+  Divider,
+  Link,
+  styled,
+  Theme,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from '@mui/material'
 import { useTranslation } from 'next-i18next'
 
 import { AddressDetailsView } from '@/components/checkout'
+import { PromoCodeBadge } from '@/components/common'
 import { useCheckoutStepContext } from '@/context'
 import { checkoutGetters } from '@/lib/getters'
 
@@ -11,6 +23,20 @@ import type { Order } from '@/lib/gql/types'
 
 interface OrderReviewProps {
   checkout: Order
+}
+
+const style = {
+  accordion: {
+    ':before': {
+      backgroundColor: 'transparent',
+    },
+    boxShadow: 0,
+    borderRadius: 0,
+  },
+
+  accordionDetails: {
+    p: 0,
+  },
 }
 
 const StyledOrderReview = styled(Box)(({ theme }: { theme: Theme }) => ({
@@ -49,6 +75,18 @@ const OrderReview = (props: OrderReviewProps) => {
   const { shippingPhoneHome, shippingAddress } = shippingDetails
   const { billingAddress } = billingDetails
 
+  const shippingPersonalDetails = {
+    firstName: shippingDetails.firstName,
+    middleNameOrInitial: shippingDetails.middleNameOrInitial,
+    lastNameOrSurname: shippingDetails.lastNameOrSurname,
+  }
+
+  const billingPersonalDetails = {
+    firstName: billingDetails.firstName,
+    middleNameOrInitial: billingDetails.middleNameOrInitial,
+    lastNameOrSurname: billingDetails.lastNameOrSurname,
+  }
+
   const handleEditAction = (event: MouseEvent<HTMLElement>) => {
     const redirectStepIndex = steps.findIndex(
       (step: string) => step === event.currentTarget.getAttribute('data-step')
@@ -56,83 +94,127 @@ const OrderReview = (props: OrderReviewProps) => {
     setActiveStep(redirectStepIndex)
   }
 
+  const handleAccordionChange = (
+    _event: React.SyntheticEvent<Element, Event>,
+    expanded: boolean
+  ) => {
+    if (!expanded) return
+  }
+
   return (
-    <StyledOrderReview>
-      <StyledRow sx={{ marginBottom: 0 }}>
-        <StyledLabel variant="h2" sx={{ fontWeight: 600 }}>
+    <Accordion
+      data-testid="accordian"
+      onChange={handleAccordionChange}
+      disableGutters
+      sx={{ ...style.accordion }}
+      defaultExpanded={true}
+    >
+      <AccordionSummary
+        data-testid="accordian-summery"
+        expandIcon={<ExpandMore />}
+        aria-controls="panel1bh-content"
+        id="panel1bh-header"
+        sx={(theme) => ({
+          backgroundColor: theme?.palette.grey[100],
+          paddingLeft: '20px',
+          paddingY: '5px',
+        })}
+      >
+        <Typography variant="h2" fontWeight={600}>
           {t('order-review')}
-        </StyledLabel>
-      </StyledRow>
-      <Divider sx={{ marginTop: '1.25rem', marginBottom: '1.813rem' }} />
-      <StyledRow sx={{ marginBottom: '0.563rem' }}>
-        <Typography variant="subtitle2" fontWeight={600}>
-          {t('personal-details')}
         </Typography>
-        <StyledActions
-          data-testid={'edit-personal-details'}
-          data-step={t('details')}
-          variant="caption"
-          onClick={handleEditAction}
-        >
-          {t('edit')}
-        </StyledActions>
-      </StyledRow>
-      <StyledRow sx={{ display: 'inline' }}>
-        <Typography variant="body1">{userName} </Typography>
-        <Typography variant="body1">{shippingPhoneHome}</Typography>
-      </StyledRow>
+      </AccordionSummary>
 
-      <StyledRow sx={{ marginTop: '2.375rem' }}>
-        <AddressDetailsView {...shippingAddress} withoutRadioTitle={t('shipping-details')} />
-        <StyledActions
-          data-testid={'edit-shipping-details'}
-          data-step={t('shipping')}
-          variant="caption"
-          color="text.primary"
-          onClick={handleEditAction}
-        >
-          {t('edit')}
-        </StyledActions>
-      </StyledRow>
+      <AccordionDetails data-testid="accordian-details" sx={{ ...style.accordionDetails }}>
+        <Divider sx={{ marginX: '20px' }} />
+        <StyledOrderReview>
+          <StyledRow sx={{ marginBottom: '0.563rem' }}>
+            <Typography variant="subtitle2" fontWeight={600}>
+              {t('personal-details')}
+            </Typography>
+            <StyledActions
+              data-testid={'edit-personal-details'}
+              data-step={t('details')}
+              variant="caption"
+              onClick={handleEditAction}
+            >
+              {t('edit')}
+            </StyledActions>
+          </StyledRow>
 
-      <StyledRow>
-        <AddressDetailsView {...billingAddress} withoutRadioTitle={t('billing-address')} />
-        <StyledActions
-          data-testid={'edit-billing-address'}
-          data-step={t('payment')}
-          variant="caption"
-          color="text.primary"
-          onClick={handleEditAction}
-        >
-          {t('edit')}
-        </StyledActions>
-      </StyledRow>
-      <StyledRow sx={{ marginBottom: '1rem' }}>
-        <StyledLabel variant="h4">{t('payment-method')}</StyledLabel>
-        <StyledActions
-          data-testid={'edit-payment-method'}
-          data-step={t('payment')}
-          variant="caption"
-          color="text.primary"
-          onClick={handleEditAction}
-        >
-          {t('edit')}
-        </StyledActions>
-      </StyledRow>
+          <StyledRow sx={{ display: 'inline' }}>
+            <Typography variant="body1">{userName} </Typography>
+            <Typography variant="body1">{shippingPhoneHome}</Typography>
+          </StyledRow>
 
-      {paymentMethods.map((paymentMethod) => (
-        <StyledRow
-          sx={{ display: 'inline' }}
-          key={`${paymentMethod.cardNumberPartOrMask}-${paymentMethod.expiry}`}
-        >
-          <Typography variant="body1">{paymentMethod.cardType}</Typography>
-          <Typography variant="body1">{paymentMethod.cardNumberPartOrMask}</Typography>
-          <Typography variant="body1">{paymentMethod.expiry} XXX</Typography>
-        </StyledRow>
-      ))}
+          <StyledRow sx={{ marginTop: '2.375rem' }}>
+            <AddressDetailsView
+              {...shippingPersonalDetails}
+              {...shippingAddress}
+              withoutRadioTitle={t('shipping-details')}
+            />
+            <StyledActions
+              data-testid={'edit-shipping-details'}
+              data-step={t('shipping')}
+              variant="caption"
+              color="text.primary"
+              onClick={handleEditAction}
+            >
+              {t('edit')}
+            </StyledActions>
+          </StyledRow>
 
-      {/* tobe:  promocode component */}
-    </StyledOrderReview>
+          <StyledRow>
+            <AddressDetailsView
+              {...billingPersonalDetails}
+              {...billingAddress}
+              withoutRadioTitle={t('billing-address')}
+            />
+            <StyledActions
+              data-testid={'edit-billing-address'}
+              data-step={t('payment')}
+              variant="caption"
+              color="text.primary"
+              onClick={handleEditAction}
+            >
+              {t('edit')}
+            </StyledActions>
+          </StyledRow>
+
+          <StyledRow sx={{ marginBottom: '1rem' }}>
+            <StyledLabel variant="h4">{t('payment-method')}</StyledLabel>
+            <StyledActions
+              data-testid={'edit-payment-method'}
+              data-step={t('payment')}
+              variant="caption"
+              color="text.primary"
+              onClick={handleEditAction}
+            >
+              {t('edit')}
+            </StyledActions>
+          </StyledRow>
+
+          {paymentMethods.map((paymentMethod) => (
+            <StyledRow
+              sx={{ display: 'inline' }}
+              key={`${paymentMethod.cardNumberPartOrMask}-${paymentMethod.expiry}`}
+            >
+              <Typography variant="body1">{paymentMethod.cardType}</Typography>
+              <Typography variant="body1">{paymentMethod.cardNumberPartOrMask}</Typography>
+              <Typography variant="body1">{paymentMethod.expiry} XXX</Typography>
+            </StyledRow>
+          ))}
+
+          <Divider sx={{ marginTop: '1.25rem', marginBottom: '1.813rem' }} />
+          <PromoCodeBadge
+            onApplyCouponCode={() => ''}
+            onRemoveCouponCode={() => ''}
+            promoList={[]}
+            promoError={false}
+          />
+        </StyledOrderReview>
+      </AccordionDetails>
+    </Accordion>
   )
 }
 
