d19738d788667f4a3e6c9494c3e68249a526899f
kibo-Kripa <kripa.venkateswaran@kibocommerce.com>
2022-08-02 16:18:53 -0600

    resolved all PR comments on test cases

--- components/home/Carousel/KiboHeroCarousel.spec.tsx
+++ components/home/Carousel/KiboHeroCarousel.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { composeStories } from '@storybook/testing-react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import mediaQuery from 'css-mediaquery'
 
 import * as stories from './KiboHeroCarousel.stories'
@@ -21,78 +21,74 @@ const createMatchMedia = (width: number) => (query: string) => ({
 
 describe('checkout Component', () => {
   const setup = () => render(<Common {...Common.args} />)
+  it('should render next', () => {
+    setup()
+    const nexticon = screen.getAllByTestId('FiberManualRecordIcon')
+    expect(nexticon[0]).toBeVisible()
+  })
 
-  describe('checkout', () => {
-    it('should render next', () => {
-      setup()
-      const nexticon = screen.getAllByTestId('FiberManualRecordIcon')
-      expect(nexticon[0]).toBeVisible()
+  it('should render component', () => {
+    setup()
+    const title = screen.getAllByText(/Save up to 50%/i)
+    expect(title[0]).toBeInTheDocument()
+  })
+
+  it('should render component', () => {
+    setup()
+
+    const carouselValues = Common?.args?.carouselItem || []
+    const subtitle = screen.getAllByText(carouselValues[0].subtitle!)
+    const description = screen.getAllByText(carouselValues[0].description!)
+    const buttonText = screen.getAllByText(carouselValues[0].buttonText!)
+
+    expect(subtitle[0]).toBeInTheDocument()
+    expect(description[0]).toBeInTheDocument()
+    expect(buttonText[0]).toBeInTheDocument()
+  })
+
+  it('should render button', () => {
+    setup()
+    const carouselValues = Common?.args?.carouselItem || []
+    expect(screen.getByRole('button', { name: carouselValues[0].buttonText })).toBeInTheDocument()
+  })
+
+  it('should render product image', () => {
+    setup()
+
+    const image = screen.getAllByTestId('product-image')
+    expect(image[0]).toHaveAttribute('alt', 'image Alt text')
+  })
+
+  it('should move to next slide', () => {
+    setup()
+    expect(screen.getByRole('button', { name: 'Next' })).toBeVisible()
+  })
+
+  it('should move to previous slide', () => {
+    setup()
+    expect(screen.getByRole('button', { name: 'Previous' })).toBeVisible()
+  })
+
+  it('should render mobileurl on mobile screen ', async () => {
+    window.matchMedia = createMatchMedia(500)
+    setup()
+    const carouselValues = Common?.args?.carouselItem || []
+
+    const mobileImage = screen.getAllByRole('img')
+    await waitFor(() => {
+      expect(mobileImage[0]).toHaveAttribute('src', carouselValues[0].mobileImageUrl)
     })
+  })
 
-    it('should render component', () => {
-      setup()
-      const title = screen.getAllByText(/Save up to 50%/i)
-      expect(title[0]).toBeInTheDocument()
-    })
+  it('should render imageUrl on desktop screen', async () => {
+    window.matchMedia = createMatchMedia(1000)
+    setup()
 
-    it('should render component', () => {
-      setup()
+    const carouselValues = Common?.args?.carouselItem || []
 
-      const carouselValues = Common?.args?.carouselItem || []
-      const subtitle = screen.getAllByText(carouselValues[0].subtitle!)
-      const description = screen.getAllByText(carouselValues[0].description!)
-      const buttonText = screen.getAllByText(carouselValues[0].buttonText!)
-
-      expect(subtitle[0]).toBeInTheDocument()
-      expect(description[0]).toBeInTheDocument()
-      expect(buttonText[0]).toBeInTheDocument()
-    })
-
-    it('should render button', () => {
-      setup()
-      const carouselValues = Common?.args?.carouselItem || []
-      expect(screen.getByRole('button', { name: carouselValues[0].buttonText })).toBeInTheDocument()
-    })
-
-    it('should render product image', () => {
-      setup()
-
-      const image = screen.getAllByTestId('product-image')
-      expect(image[0]).toHaveAttribute('alt', 'image Alt text')
-    })
-
-    it('should move to next slide', () => {
-      setup()
-      expect(screen.getByRole('button', { name: 'Next' })).toBeVisible()
-    })
-
-    it('should move to previous slide', () => {
-      setup()
-      expect(screen.getByRole('button', { name: 'Previous' })).toBeVisible()
-    })
-
-    it('mobile', async () => {
-      window.matchMedia = createMatchMedia(500)
-      setup()
-      const carouselValues = Common?.args?.carouselItem || []
-
-      const description = screen.getAllByText(carouselValues[0].description!)
-      const buttontext = screen.getAllByText(carouselValues[0].buttonText!)
-
-      expect(description[0]).toHaveStyle('font-size: 0.75rem')
-      expect(buttontext[0]).toHaveStyle('font-size: 0.5rem')
-    })
-
-    it('Desktop', async () => {
-      window.matchMedia = createMatchMedia(1000)
-      setup()
-      const carouselValues = Common?.args?.carouselItem || []
-
-      const description = screen.getAllByText(carouselValues[0].description!)
-      const buttontext = screen.getAllByText(carouselValues[0].buttonText!)
-
-      expect(description[0]).toHaveStyle('font-size: 1rem')
-      expect(buttontext[0]).toHaveStyle('font-size: 1rem')
+    const desktopImage = screen.getAllByRole('img')
+    await waitFor(() => {
+      expect(desktopImage[0]).toHaveAttribute('src', carouselValues[0].imageUrl)
     })
   })
 })
