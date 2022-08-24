1866dad22f9d59fec8e7c46a730f6d4913bb9b95
amolpawar-kibo <amol.pawar@kibocommerce.com>
2022-08-24 12:43:56 +0530

    feat(my account): add address book story

--- components/my-account/AddressBook/AddressBook.stories.tsx
+++ components/my-account/AddressBook/AddressBook.stories.tsx
@@ -3,12 +3,28 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import AddressBook from './AddressBook'
+import { userResponseMock } from '@/__mocks__/stories/userMock'
+import { AuthContext, AuthContextType } from '@/context/'
 
 export default {
   title: 'My Account / Address Book',
   component: AddressBook,
 } as ComponentMeta<typeof AddressBook>
 
-const Template: ComponentStory<typeof AddressBook> = () => <AddressBook />
+const userContextValues: AuthContextType = {
+  isAuthenticated: true,
+  login: () => console.log('Login'),
+  user: userResponseMock,
+  createAccount: () => console.log('createAccount'),
+  setAuthError: () => console.log('setAuthError'),
+  authError: '',
+  logout: () => console.log('logout'),
+}
+
+const Template: ComponentStory<typeof AddressBook> = () => (
+  <AuthContext.Provider value={userContextValues}>
+    <AddressBook />
+  </AuthContext.Provider>
+)
 
 export const Common = Template.bind({})
