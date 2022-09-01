a08b9e7791a6b36ff50c670e37653f92108944c3
kibo-geetanshuchhabra <geetanshu.chhabra@kibocommerce.com>
2022-09-01 13:12:58 +0530

    fix package-lock.json file

--- components/home/CmsHomePageProducts/CmsHomePageProducts.tsx
+++ components/home/CmsHomePageProducts/CmsHomePageProducts.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { Grid } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 
 import { ProductRecommendations } from '@/components/product'
 import { ProductRecommendationsProps } from '@/components/product/ProductRecommendations/ProductRecommendations'
@@ -17,18 +17,20 @@ const CmsHomePageProducts = (props: {
   topSellings: ProductRecommendationsProps
 }) => {
   return (
-    <Grid sx={{ ...styles.gridContainer }} container columnSpacing={{ md: 5 }}>
-      <Grid item sm={12} md={6}>
-        {props?.recentlyViewed?.productCodes?.length > 0 && (
-          <ProductRecommendations {...props.recentlyViewed} />
-        )}
+    <Container maxWidth={'xl'}>
+      <Grid sx={{ ...styles.gridContainer }} container columnSpacing={{ md: 5 }}>
+        <Grid item sm={12} md={6}>
+          {props?.recentlyViewed?.productCodes?.length > 0 && (
+            <ProductRecommendations {...props.recentlyViewed} />
+          )}
+        </Grid>
+        <Grid item sm={12} md={6}>
+          {props?.topSellings?.productCodes?.length > 0 && (
+            <ProductRecommendations {...props.topSellings} />
+          )}
+        </Grid>
       </Grid>
-      <Grid item sm={12} md={6}>
-        {props?.topSellings?.productCodes?.length > 0 && (
-          <ProductRecommendations {...props.topSellings} />
-        )}
-      </Grid>
-    </Grid>
+    </Container>
   )
 }
 
