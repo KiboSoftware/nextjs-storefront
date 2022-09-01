64b9bd9a136cd4834e6c69c3f5349da8a7c6f2d0
kibo-Kripa <kripa.venkateswaran@kibocommerce.com>
2022-09-01 16:43:36 -0600

    bug fix

--- components/my account/my profile/MyProfile.spec.tsx
+++ components/my account/my profile/MyProfile.spec.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { renderHook } from '@testing-library/react-hooks'
 
 import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+
 import * as stories from './MyProfile.stories'
-import { useAuthContext } from '@/context'
 
 const { Common } = composeStories(stories)
 
@@ -22,15 +21,6 @@ describe('[components] MyProfile', () => {
     }
   }
 
-  it('render consumer data', async () => {
-    renderHook(async () => {
-      setup()
-      const { user, isAuthenticated } = useAuthContext()
-      // const firstName = screen.getByText(user?.firstName as string)
-      // const lastName = screen.getByText(/customer-name/i)
-      // expect(firstName).toBeVisible()
-    })
-  })
   it('should render component', () => {
     setup()
 
@@ -39,13 +29,10 @@ describe('[components] MyProfile', () => {
     const passwordText = screen.getByText(/password/i)
     const EditName = screen.getAllByText(/edit/i)
 
-    // const customerName =  screen.getByText('customer-name')
-
     expect(customerNameText).toBeVisible()
     expect(emailText).toBeVisible()
     expect(passwordText).toBeVisible()
     expect(EditName[0]).toBeVisible()
-    // expect(customerName).toBeVisible()
   })
   it('should render edit', async () => {
     const { userAction } = setup()
@@ -73,42 +60,3 @@ describe('[components] MyProfile', () => {
     expect(editPassword).toBeVisible()
   })
 })
-
-// describe('checkout Component', () => {
-//   const setup = () => render(<Common {...Common?.args} />)
-//   describe('checkout', () => {
-//     it('should render component', () => {
-//       setup()
-//       const Edit = screen.getAllByText('Edit')
-//       const customerName = screen.getAllByText('Customer Name')
-//       const emailAddress = screen.getAllByText('Email')
-//       const phoneNumber = screen.getAllByText('phoneNumber')
-//       const password = screen.getAllByText('Password')
-
-//       expect(Edit[0]).toBeVisible()
-//     })
-//     it('should render edit', () => {
-//       setup()
-//       const EditName = screen.getAllByText('Edit')
-//       fireEvent.click(EditName[0])
-//       const editCustomerName = screen.getByText('Edit Customer Name')
-//       expect(editCustomerName).toBeVisible()
-
-//       const cancelName = screen.getByText('Cancel')
-//       fireEvent.click(cancelName)
-
-//       const Editemail = screen.getAllByText('Edit')
-//       fireEvent.click(Editemail[1])
-//       const editEmail = screen.getByText('Edit Email')
-//       expect(editEmail).toBeVisible()
-
-//       const cancelEmail = screen.getByText('Cancel')
-//       fireEvent.click(cancelEmail)
-
-//       const Editpassword = screen.getAllByText('Edit')
-//       fireEvent.click(Editpassword[3])
-//       const editPassword = screen.getByText('Edit Password')
-//       expect(editPassword).toBeVisible()
-//     })
-//   })
-// })
