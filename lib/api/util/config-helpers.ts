ea7608a8c077b46588a58f3c9f2b5cc1b579c112
kibo-kevinwatts <85258296+kibo-kevinwatts@users.noreply.github.com>
2022-09-07 18:18:31 -0500

    fix(config helper): api config helper to use next config for apihost if available

--- lib/api/util/config-helpers.ts
+++ lib/api/util/config-helpers.ts
@@ -1,17 +1,22 @@
+import getConfig from 'next/config'
+
 import { UserAuthTicket } from '@kibocommerce/graphql-client'
+
+const { publicRuntimeConfig } = getConfig()
+const apiHost = publicRuntimeConfig.apiHost || process.env.KIBO_API_HOST
 
 const protocolRegx = new RegExp(/https?:\/\//)
 const addProtocolToHost = (hostname: string | undefined) =>
   hostname && !hostname.match(protocolRegx) ? `https://${hostname}` : hostname
 
-export const getGraphqlUrl = () => `${addProtocolToHost(process.env.KIBO_API_HOST)}/graphql`
+export const getGraphqlUrl = () => `${addProtocolToHost(apiHost)}/graphql`
 
 export const getApiConfig = () => {
   return {
+    apiHost: apiHost as string,
     clientId: process.env.KIBO_CLIENT_ID as string,
     sharedSecret: process.env.KIBO_SHARED_SECRET as string,
     authHost: process.env.KIBO_AUTH_HOST as string,
-    apiHost: process.env.KIBO_API_HOST as string,
   }
 }
 
