ff9bc05a03fd54ee093abf7e652f340ae29c25a5
kibo-geetanshuchhabra <geetanshu.chhabra@kibocommerce.com>
2022-09-08 15:10:28 +0530

    fix code smell

--- components/layout/Footer/Footer.spec.tsx
+++ components/layout/Footer/Footer.spec.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/testing-react'
-import { render, screen, within } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import '@testing-library/jest-dom'
