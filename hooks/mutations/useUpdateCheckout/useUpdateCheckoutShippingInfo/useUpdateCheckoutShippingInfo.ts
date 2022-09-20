3b7766d8c8f02648dab48be3a83f411400713679
Susanta Samanta <susanta.samanta@blueconchtech.com>
2022-09-20 20:53:45 +0530

    update cleanup

--- hooks/mutations/useUpdateCheckout/useUpdateCheckoutShippingInfo/useUpdateCheckoutShippingInfo.ts
+++ hooks/mutations/useUpdateCheckout/useUpdateCheckoutShippingInfo/useUpdateCheckoutShippingInfo.ts
@@ -2,6 +2,10 @@ import { useMutation, useQueryClient } from 'react-query'
 
 import { makeGraphQLClient } from '@/lib/gql/client'
 import { setShippingInfo } from '@/lib/gql/mutations'
+import {
+  buildCheckoutShippingParams,
+  CheckoutShippingParams,
+} from '@/lib/helpers/buildCheckoutShippingParams'
 import { checkoutKeys } from '@/lib/react-query/queryKeys'
 
 import type { FulfillmentInfoInput } from '@/lib/gql/types'
@@ -11,8 +15,10 @@ export interface ShippingInfo {
   fulfillmentInfoInput: FulfillmentInfoInput
 }
 
-const updateShippingInfo = async (shippingInfo: ShippingInfo) => {
+const updateShippingInfo = async (params: CheckoutShippingParams) => {
   const client = makeGraphQLClient()
+
+  const shippingInfo: ShippingInfo = buildCheckoutShippingParams(params)
 
   const response = await client.request({
     document: setShippingInfo,
