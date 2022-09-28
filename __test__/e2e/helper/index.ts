d7eafd3569ecd8d0e94ee129e750958990f72ba8
TriptiVerma <tripti.verma@kibocommerce.com>
2022-09-28 11:45:37 +0530

    fix: reverted any to UserEvent import

--- __test__/e2e/helper/index.ts
+++ __test__/e2e/helper/index.ts
@@ -1,6 +1,7 @@
 import { screen, waitFor, within } from '@testing-library/react'
+import { UserEvent } from '@testing-library/user-event/dist/types/setup'
 
-export const addNewCard = async (user: any) => {
+export const addNewCard = async (user: UserEvent) => {
   await addCardDetails(user)
   await addBillingAddress(user)
 
@@ -15,7 +16,7 @@ export const addNewCard = async (user: any) => {
   await user.click(saveMethod)
 }
 
-const addCardDetails = async (user: any) => {
+const addCardDetails = async (user: UserEvent) => {
   // Card form values
   const cardNumber = screen.getByRole('textbox', {
     name: /card-number/i,
@@ -33,7 +34,7 @@ const addCardDetails = async (user: any) => {
   await user.tab()
 }
 
-const addBillingAddress = async (user: any) => {
+const addBillingAddress = async (user: UserEvent) => {
   const firstName = screen.getByRole('textbox', { name: /first-name/i })
   const lastNameOrSurname = screen.getByRole('textbox', { name: /last-name-or-sur-name/i })
   const address1 = screen.getByRole('textbox', { name: /address1/i })
