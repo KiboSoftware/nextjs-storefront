62576fb45e5ad28978517608e1961e954d9e7ea4
kibo-geetanshuchhabra <geetanshu.chhabra@kibocommerce.com>
2022-10-07 23:07:48 +0530

    remove duplicate code

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,7 @@ import type { GetServerSidePropsContext } from 'next'
 interface HomePageProps {
   cmsPage: any
 }
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { locale } = context
-  const categoriesTree: CategoryTreeResponse = await getCategoryTree()
-
+const getCmsHomePageData = async () => {
   const cmsPage = await getPage({
     contentTypeUid: 'home_page',
     referenceFieldPath: [
@@ -29,6 +25,14 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     ],
     entryUrl: '',
   })
+  return cmsPage
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { locale } = context
+  const categoriesTree: CategoryTreeResponse = await getCategoryTree()
+
+  const cmsPage = await getCmsHomePageData()
   return {
     props: {
       categoriesTree,
@@ -41,26 +45,16 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 const Home: NextPageWithLayout<HomePageProps> = (props) => {
   const { cmsPage } = props
   const [cmsPages, setCmsPage] = useState(cmsPage)
-  async function fetchData() {
+  const fetchData = async () => {
     try {
-      const cmsPage = await getPage({
-        contentTypeUid: 'home_page',
-        referenceFieldPath: [
-          'page_components.hero_carousel.hero_carousel_items',
-          'page_components.large_promo_blocks.large_promo_blocks',
-          'page_components.small_promo_blocks.small_promo_blocks',
-        ],
-        entryUrl: '',
-      })
+      const cmsPage = await getCmsHomePageData()
       setCmsPage(cmsPage)
     } catch (error) {
       console.error(error)
     }
   }
   useEffect(() => {
-    console.log('useeffect home page')
     onEntryChange(() => {
-      console.log('onentrychange')
       fetchData()
     })
   }, [])
