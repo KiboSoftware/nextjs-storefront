b67216acec54cf7b15a5db8f83f0fe8046cc9517
Susanta Samanta <susanta.samanta@blueconchtech.com>
2022-10-19 15:12:04 +0530

    remove preview from sonar coverage check

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,6 @@ module.exports = {
     'hooks/**/*.{js,jsx,ts,tsx}',
     'pages/**/*.{js,jsx,ts,tsx}',
     '!pages/_middleware.ts',
-    '!pages/api/contentful',
-    '!components/PreviewMode',
   ],
   moduleNameMapper: {
     // Handle CSS imports (with CSS modules)
