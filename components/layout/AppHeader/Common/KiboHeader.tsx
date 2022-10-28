78ab5c49214f80ef1cad7c1e2f27578f54d47d39
Chandradeepta <chandradeepta.laha@kibocommerce.com>
2022-10-28 13:56:06 +0530

    chore: removing unused imports

--- components/layout/AppHeader/Common/KiboHeader.tsx
+++ components/layout/AppHeader/Common/KiboHeader.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 
 import SearchIcon from '@mui/icons-material/Search'
 import {
