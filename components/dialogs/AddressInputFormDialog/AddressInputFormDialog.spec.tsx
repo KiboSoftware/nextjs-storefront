1b7be82fa26024501b99deffce7c9df9b05cfe81
Sahil Arora <Sahil.Arora@blueconchtech.com>
2022-11-04 16:10:25 +0530

    new test case added

--- components/dialogs/AddressInputFormDialog/AddressInputFormDialog.spec.tsx
+++ components/dialogs/AddressInputFormDialog/AddressInputFormDialog.spec.tsx
@@ -1,5 +1,6 @@
 import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import * as stories from './AddressInputFormDialog.stories'
 
@@ -8,15 +9,16 @@ const { Common } = composeStories(stories)
 const addressFormMock = () => <div data-testid="address-input-form-dialog" />
 jest.mock('@/components/common/AddressForm/AddressForm', () => addressFormMock)
 
-const renderComponent = () => {
-  return render(<Common {...Common.args} />)
-}
-
 describe('[components]  AddressInputForm Dialog', () => {
-  const setup = () => renderComponent()
+  const setup = (params = {}) => {
+    const user = userEvent.setup()
+    render(<Common {...params} />)
+
+    return { user }
+  }
 
   it('should render component', () => {
-    setup()
+    setup({ ...Common.args })
 
     const closeIcon = screen.getByRole('button', { name: 'close' })
     const addressInputFormDialogComponent = screen.getByTestId('address-input-form-dialog')
@@ -26,15 +28,23 @@ describe('[components]  AddressInputForm Dialog', () => {
   })
 
   it('should have a title', () => {
-    setup()
+    setup({ ...Common.args })
 
     const dialogHeading = screen.getByRole('heading', { name: 'Add new address' })
 
     expect(dialogHeading).toBeVisible()
   })
 
+  it('should disable save button', () => {
+    setup({ ...Common.args, isAddressFormValid: false })
+
+    const saveButton = screen.getByRole('button', { name: 'save' })
+
+    expect(saveButton).toBeDisabled()
+  })
+
   it('should render save and cancel button', () => {
-    setup()
+    setup({ ...Common.args })
 
     const saveButton = screen.getByRole('button', { name: 'save' })
     const cancelButton = screen.getByRole('button', { name: 'cancel' })
