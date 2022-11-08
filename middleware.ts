84985295fb2cc3087670ff3069ac4e7a52e20c9f
Susanta Samanta <susanta.samanta@blueconchtech.com>
2022-11-08 20:56:36 +0530

    comment buffer code from middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
 import { NextResponse, NextRequest } from 'next/server'
 
-import { decodeParseCookieValue } from './lib/helpers/cookieHelper'
+// import { decodeParseCookieValue } from './lib/helpers/cookieHelper'
 
-const checkIsAutheticated = (req: NextRequest) => {
-  const cookie = req.headers.get('cookie')
+// const checkIsAutheticated = (req: NextRequest) => {
+//   const cookie = req.headers.get('cookie')
 
-  const decodedCookie = decodeParseCookieValue(cookie?.split('kibo_at=')[1])
-  return decodedCookie?.userId
-}
+//   const decodedCookie = decodeParseCookieValue(cookie?.split('kibo_at=')[1])
+//   return decodedCookie?.userId
+// }
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/my-account')) {
-    if (checkIsAutheticated(request)) {
-      return NextResponse.next()
-    }
+    // if (checkIsAutheticated(request)) {
+    //   return NextResponse.next()
+    // }
     const homeUrl = new URL('/', request.url)
     return NextResponse.redirect(homeUrl)
   }
