b8c72d8a2b77cda9d236b67529f552130702cd15
Susanta Samanta <susanta.samanta@blueconchtech.com>
2022-11-09 21:37:31 +0530

    fix: vercel deployment crash

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
     localeDetection: true,
+    localePath: path.resolve('./public/locales'),
   },
 }
