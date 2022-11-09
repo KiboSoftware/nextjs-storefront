4f678618a02639cffd6a735dabfb1a9815a4630d
Susanta Samanta <92077348+kibo-susantasamanta@users.noreply.github.com>
2022-11-09 10:08:41 +0530

    Delete middleware.ts
    remove middleware file

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/my-account')) {
-    const homeUrl = new URL('/', request.url)
-    return NextResponse.redirect(homeUrl)
-  }
-}
