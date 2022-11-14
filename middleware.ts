8b9771a0969493138cea945bc841928250e7ab69
Susanta Samanta <susanta.samanta@blueconchtech.com>
2022-11-14 19:18:04 +0530

    test change

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server'
 
-import { decodeParseCookieValue } from './lib/helpers/cookieHelper'
-
 const checkIsAutheticated = (req: NextRequest) => {
-  const cookie = req.headers.get('cookie')
-
-  const decodedCookie = decodeParseCookieValue(cookie?.split('kibo_at=')[1])
-  return decodedCookie?.userId
+  return true
 }
 
 export function middleware(request: NextRequest) {
