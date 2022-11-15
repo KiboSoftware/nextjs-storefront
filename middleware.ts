a18b1617065f24da7272508d40437dabb74e7e0f
Susanta Samanta <92077348+kibo-susantasamanta@users.noreply.github.com>
2022-11-15 20:08:56 +0530

    Create middleware.ts
    change directory fot middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+const checkIsAutheticated = (req: NextRequest) => {
+  const cookie = req.headers.get('cookie')
+  const cookieValue = cookie?.split('kibo_at=')[1]
+  const decodedCookie = JSON.parse(atob(cookieValue as string))
+  return decodedCookie?.userId
+}
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/my-account')) {
+    if (checkIsAutheticated(request)) {
+      return NextResponse.next()
+    }
+    const homeUrl = new URL('/', request.url)
+    return NextResponse.redirect(homeUrl)
+  }
+}
