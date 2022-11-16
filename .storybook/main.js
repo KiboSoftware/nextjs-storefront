004235c22ad00346bd751b839adeaa5b187ab0e7
Susanta Samanta <92077348+kibo-susantasamanta@users.noreply.github.com>
2022-11-16 17:04:51 +0530

    update story main file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
+import { fileURLToPath } from 'url'
 const path = require('path')
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
 
 function resolve(dir) {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
   return path.join(__dirname, dir)
 }
+
 module.exports = {
   webpackFinal: async (config) => {
     // config.resolve.modules.push(path.resolve(__dirname, '../'));
