5565d3e8cc470dd5c46bc92dae23f8611e19cba3
Susanta Samanta <92077348+kibo-susantasamanta@users.noreply.github.com>
2022-11-16 17:13:48 +0530

    revert change

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,7 @@
-import { fileURLToPath } from 'url'
 const path = require('path')
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
 
 function resolve(dir) {
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = path.dirname(__filename)
   return path.join(__dirname, dir)
 }
 
