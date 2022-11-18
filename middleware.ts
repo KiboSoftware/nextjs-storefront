fa783545b7b24c0055067bac51cf5308a744434b
kibo-amolpawar <92151681+kibo-amolpawar@users.noreply.github.com>
2022-11-18 10:50:22 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -12,8 +12,7 @@ export function middleware(request: NextRequest) {
     if (checkIsAutheticated(request)) {
       return NextResponse.next()
     }
-    
-    const homeUrl = new URL('/', request.url)
-    return NextResponse.redirect(homeUrl)
   }
+   const homeUrl = new URL('/', request.url)
+    return NextResponse.redirect(homeUrl)
 }
