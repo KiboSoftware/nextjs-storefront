67f562961b0df6a8d0f06e6cc2f75af0b726ba26
kibo-sushant <89385472+kibo-sushant@users.noreply.github.com>
2022-11-21 14:02:42 +0530

    Update 2

--- middleware.ts
+++ middleware.ts
@@ -10,10 +10,7 @@ const checkIsAutheticated = (req: NextRequest) => {
 }
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/my-account')) {
-    
-
     const homeUrl = new URL('/', request.url)
     return NextResponse.redirect(homeUrl)
-  }
+ 
 }
