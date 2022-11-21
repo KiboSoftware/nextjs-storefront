88a32b6923011093c09c5de0ce229da1afed2150
kibo-sushant <sushant.jadhav@blueconchtech.com>
2022-11-21 15:36:04 +0530

    update 6

--- .storybook/middleware.js
+++
@@ -1,9 +0,0 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// const expressMiddleWare = (router) => {
-//   router.use('/_next/image', createProxyMiddleware({ target: 'http://localhost:{ NEXTJS_PORT}', changeOrigin: true }));
-// };
-
-// module.exports = expressMiddleWare;
-const middleware = () => {}
-module.exports = middleware
