bd8fb3c2d53c6d8f37dc84e2e2bf2d5a4f1d2e09
Susanta Samanta <susanta.samanta@blueconchtech.com>
2022-12-14 13:19:22 +0530

    add test

---
+++ components/page-templates/StandardCheckoutHeaderTemplate/StandardCheckoutHeaderTemplate.spec.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { composeStories } from '@storybook/testing-react'
+import { render, screen } from '@testing-library/react'
+
+import * as stories from './StandardCheckoutHeaderTemplate.stories'
+
+const { Common } = composeStories(stories)
+
+jest.mock('../../common/OrderSummary/OrderSummary', () => ({
+  __esModule: true,
+  default: () => <div data-testid="order-summary-mock" />,
+}))
+jest.mock('../../order/OrderConfirmation/OrderConfirmation', () => ({
+  __esModule: true,
+  default: () => <div data-testid="order-confirmation-mock" />,
+}))
+jest.mock('../../checkout/OrderReview/OrderReview', () => ({
+  __esModule: true,
+  default: () => <div data-testid="order-review-mock" />,
+}))
+
+jest.mock('@mui/material', () => ({
+  ...jest.requireActual('@mui/material'),
+  useMediaQuery: jest.fn().mockReturnValue(false),
+}))
+
+describe('[component] - CheckoutUITemplate', () => {
+  it('should render component', async () => {
+    render(<Common {...Common?.args} />)
+    const orderSummary = screen.getByTestId('order-summary-mock')
+    const OrderConfirmation = screen.getByTestId('order-confirmation-mock')
+    const OrderReview = screen.getByTestId('order-review-mock')
+
+    expect(orderSummary).toBeVisible()
+    expect(OrderConfirmation).toBeVisible()
+    expect(OrderReview).toBeVisible()
+  })
+})
