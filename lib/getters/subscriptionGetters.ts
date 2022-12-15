086359a4cd4f69f9571a526969cdb6505a040ebe
kibo-nehabhanushali <89385305+kibo-nehabhanushali@users.noreply.github.com>
2022-12-15 07:50:21 +0530

    feat: added mysubscription component template (#187)
    * feat: Created mysubscription component template
    
    * feat: Updated code as per review and removed security threat
    
    * Removed code smells
    
    * Added test cases
    
    * Updated code to reduce duplicate lines
    
    * Removed code smell
    
    * Resolved PR comments
    
    * Removed code smell for type
    
    * Resolved PR comments
    
    * Updated folder structure, naming as per review comment

---
+++ lib/getters/subscriptionGetters.ts
@@ -0,0 +1,38 @@
+import { format } from 'date-fns'
+
+const getSubscriberName = (subscription: any) =>
+  `${subscription?.fulfillmentInfo?.fulfillmentContact?.firstName} ${subscription?.fulfillmentInfo?.fulfillmentContact?.lastNameOrSurname}`
+
+const getAddress = (subscription: any) => subscription?.fulfillmentInfo?.fulfillmentContact?.address
+
+const getSubscriberAddress = (subscription: any) =>
+  `${getAddress(subscription)?.address1} ${getAddress(subscription)?.address2}  ${
+    getAddress(subscription)?.cityOrTown
+  } ${getAddress(subscription)?.stateOrProvince} ${getAddress(subscription)?.postalOrZipCode}  ${
+    getAddress(subscription)?.countryCode
+  }`
+
+const getSubscriptionFrequency = (subscription: any) =>
+  `${subscription?.frequency?.value} ${subscription?.frequency?.unit}`
+
+const nextOrderItemDate = (subscription: any) =>
+  format(new Date(subscription?.nextOrderDate), 'MMMM dd, yyyy')
+
+const getSubscriptionNumber = (subscription: any) => subscription?.number
+
+const getSubscriptionStatus = (subscription: any) => subscription?.status
+
+const getSubscriptionDetails = (subscription: any) => {
+  return {
+    subscriberName: getSubscriberName(subscription),
+    subscriberAddress: getSubscriberAddress(subscription),
+    subscriptionFrequency: getSubscriptionFrequency(subscription),
+    nextOrderDate: nextOrderItemDate(subscription),
+    subscriptionNumber: getSubscriptionNumber(subscription),
+    subscriptionStatus: getSubscriptionStatus(subscription),
+  }
+}
+
+export const subscriptionGetters = {
+  getSubscriptionDetails,
+}
