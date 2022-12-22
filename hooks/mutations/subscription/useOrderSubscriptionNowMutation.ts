249dd899b3913956871918601c0f7ccb5b1956e8
Chandradeepta <chandradeepta.laha@kibocommerce.com>
2022-12-22 21:05:02 +0530

    feat: adding type doc

--- hooks/mutations/subscription/useOrderSubscriptionNowMutation.ts
+++ hooks/mutations/subscription/useOrderSubscriptionNowMutation.ts
@@ -24,6 +24,19 @@ const orderSubscriptionNowMutation = async (props: OrderSubscriptionNowInputPara
   return response?.orderSubscriptionNow
 }
 
+/**
+ * [Mutation hook] useOrderSubscriptionNowMutation uses the graphQL mutation
+ *
+ * <b>orderSubscriptionNow(subscriptionId: String!): Subscription</b>
+ *
+ * Description : Order a subscription for a product
+ *
+ * Parameters passed to function orderSubscriptionNowMutation(props: OrderSubscriptionNowInputParams) => expects object of type 'OrderSubscriptionNowInputParams' containing subscriptionId
+ *
+ * On success, calls invalidateQueries on subscriptionKeys and fetches the updated result.
+ *
+ * @returns 'response?.orderSubscriptionNow' which contains object of Subscription
+ */
 export const useOrderSubscriptionNowMutation = () => {
   const queryClient = useQueryClient()
   return {
