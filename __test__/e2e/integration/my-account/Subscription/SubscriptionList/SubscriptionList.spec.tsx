d3fbe415436714a0b15d06a344ab6d54cc5991fe
TriptiVerma <tripti.verma@kibocommerce.com>
2023-01-04 13:03:56 +0530

    ICKY-798 Combined subscription list integration specs

--- __test__/e2e/integration/my-account/Subscription/SubscriptionList/SubscriptionList.spec.tsx
+++ __test__/e2e/integration/my-account/Subscription/SubscriptionList/SubscriptionList.spec.tsx
@@ -5,6 +5,7 @@ import { composeStories } from '@storybook/testing-react'
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
+import { createMockRouter } from '@/__test__/utils'
 import { renderWithQueryClient } from '@/__test__/utils/renderWithQueryClient'
 import * as stories from '@/components/my-account/Subscription/SubscriptionList/SubscriptionList.stories' // import all stories from the stories file
 import { DialogRoot, ModalContextProvider } from '@/context'
@@ -13,19 +14,32 @@ const { Common } = composeStories(stories)
 
 const setup = () => {
   const user = userEvent.setup()
+  const mockOnAccountTitleClick = jest.fn()
+  const router = createMockRouter()
+
   renderWithQueryClient(
     <ModalContextProvider>
       <DialogRoot />
-      <Common />
+      <Common {...Common.args} onAccountTitleClick={mockOnAccountTitleClick} />
     </ModalContextProvider>
   )
 
   return {
     user,
+    mockOnAccountTitleClick,
+    router,
   }
 }
 
 describe('[component] - SubscriptionList integration', () => {
+  it('should go back to my account page on clicking on back arrow icon from mySubscription Page', async () => {
+    const { user, mockOnAccountTitleClick } = setup()
+    const applyButton = screen.getByTestId(/ArrowBackIosIcon/i)
+    await user.click(applyButton)
+    expect(mockOnAccountTitleClick).toBeCalled()
+    expect(screen.getByText(/my-account/i)).toBeVisible()
+  })
+
   it('should open confirmation dialog on skip shipment button click', async () => {
     const { user } = setup()
 
