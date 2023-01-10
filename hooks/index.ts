KiboSoftware/nextjs-prismic-starter-storefront

feat: skip next subscription action with hooks

hooks/index.ts
@@ -74,3 +74,6 @@ export * from './mutations/useProfile/useUpdateUserPassword/useUpdateUserPasswor
 
 // MultiShip
 export * from './mutations/multiShip/useCreateCheckoutActionMutation/useCreateCheckoutActionMutation'
+
+//subscription
+export * from './mutations/subscription/useSkipNextSubscription/useSkipNextSubscriptionMutation'

KiboSoftware/nextjs-storefront

hooks/index.ts
@@ -82,3 +82,5 @@ export * from './mutations/useUpdateCheckoutItemDestinationMutations/useUpdateCh
 
 // Subscription
 export * from './mutations/useSubscription/useEditSubscriptionFrequencyMutation/useEditSubscriptionFrequencyMutation'
+//subscription
+export * from './mutations/useSubscription/useSkipNextSubscription/useSkipNextSubscriptionMutation'