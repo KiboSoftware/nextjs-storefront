1424cef5cd548e83adbb32e464400c42b9927674
Kibo-TriptiV <tripti.verma@kibocommerce.com>
2023-02-13 13:40:28 +0530

    icky-837 fixed linter issues

--- components/dialogs/Store/StoreLocatorDialog/StoreLocatorDialog.tsx
+++ components/dialogs/Store/StoreLocatorDialog/StoreLocatorDialog.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 
 import FmdGoodIcon from '@mui/icons-material/FmdGood'
 import { Box, Button, Typography } from '@mui/material'
-import getConfig from 'next/config'
 import { useTranslation } from 'next-i18next'
+import getConfig from 'next/config'
 
 import { KiboDialog } from '@/components/common'
 import { SearchStore } from '@/components/dialogs'
@@ -125,7 +125,8 @@ const StoreLocatorDialog = (props: StoreLocatorProps) => {
           variant="contained"
           disabled={showProductAndInventory && locationInventory?.length === 0}
           onClick={() => {
-            handleSetStore(selectedStore), closeModal()
+            handleSetStore(selectedStore)
+            closeModal()
           }}
         >
           {t('set-store')}
