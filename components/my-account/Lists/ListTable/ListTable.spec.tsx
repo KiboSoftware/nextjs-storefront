02df888e3f17883f21b8b445d5f83f45996cad62
Krishna Mohan <krishna.m@ignitiv.com>
2023-07-14 18:46:09 +0530

    feature- base test case for ListTabl

---
+++ components/my-account/Lists/ListTable/ListTable.spec.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { composeStories } from '@storybook/testing-react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import * as stories from './ListTable.stories'
+
+const { Table } = composeStories(stories)
+
+function setup() {
+  const user = userEvent.setup()
+  render(<Table {...Table.args} />)
+  return { user }
+}
+
+describe('[component] - ListTable', () => {
+  it('should render the list table', () => {
+    setup()
+    const table = screen.getByRole('table')
+    expect(table).toBeVisible()
+  })
+  it('should check for number of rows in list table', () => {
+    setup()
+    const tableRows = screen.getAllByRole('row')
+    if (Table.args?.rows) {
+      // adding one to length because using TableRow in the TableHead
+      expect(tableRows.length).toEqual(Table.args.rows.length + 1)
+    }
+  })
+  it('should open edit form for the list', () => {
+    setup()
+    const editBtn = screen.getAllByTestId('editBtn')
+    console.log(editBtn)
+  })
+})
