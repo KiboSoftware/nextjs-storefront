ec5069b8ac6d499efa20a43f5c9d3d2384af36ec
Krishna Mohan <krishna.m@ignitiv.com>
2023-07-16 20:58:26 +0530

    refactor: ListsTable

--- components/my-account/Lists/ListTable/ListTable.spec.tsx
+++ components/my-account/Lists/ListTable/ListTable.spec.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
