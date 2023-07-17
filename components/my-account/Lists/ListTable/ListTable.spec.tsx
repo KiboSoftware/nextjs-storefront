cfb09aef6b0ba02f0002fc3cfefb1c27975f800e
Krishna Mohan <krishna.m@ignitiv.com>
2023-07-17 17:48:39 +0530

    feature: add testcases for btns in different view

--- components/my-account/Lists/ListTable/ListTable.spec.tsx
+++ components/my-account/Lists/ListTable/ListTable.spec.tsx
@@ -3,10 +3,22 @@ import React from 'react'
 import { composeStories } from '@storybook/testing-react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import mediaQuery from 'css-mediaquery'
 
 import * as stories from './ListTable.stories'
 
 const { Table } = composeStories(stories)
+
+const createMatchMedia = (width: number) => (query: string) => ({
+  matches: mediaQuery.match(query, { width }),
+  addListener: () => jest.fn(),
+  removeListener: () => jest.fn(),
+  media: query,
+  onchange: null,
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+})
 
 function setup() {
   const user = userEvent.setup()
@@ -28,5 +40,21 @@ describe('[component] - ListTable', () => {
       expect(tableRows.length).toEqual(Table.args.rows.length + 1)
     }
     tableRows.forEach((row) => expect(row).toBeVisible())
+  })
+  it('should check for buttons in rows to be visible in desktop view ', () => {
+    window.matchMedia = createMatchMedia(1024)
+    setup()
+    const editBtns = screen.getAllByTestId('editBtn')
+    const deleteBtns = screen.getAllByTestId('deleteBtn')
+    const copyBtns = screen.getAllByTestId('copyBtn')
+    const initiateQuoteBtns = screen.getAllByTestId('initiateQuoteBtn')
+    const addToCartBtns = screen.getAllByTestId('addToCartBtn')
+    console.log(editBtns, deleteBtns, copyBtns, initiateQuoteBtns, addToCartBtns)
+  })
+  it('should check for menu button in rows to be visible in mobile view', () => {
+    window.matchMedia = createMatchMedia(500)
+    setup()
+    const menuBtn = screen.getAllByTestId('menuBtn')
+    console.log(menuBtn)
   })
 })
