e0f22a438fa4ddd031abd8096b005ce3c9f3b211
Krishna Mohan <krishna.m@ignitiv.com>
2023-07-20 17:23:49 +0530

    feature: test cases added as per comments in ListTable

--- components/my-account/Lists/ListTable/ListTable.spec.tsx
+++ components/my-account/Lists/ListTable/ListTable.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { composeStories } from '@storybook/testing-react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import mediaQuery from 'css-mediaquery'
 
@@ -20,41 +20,77 @@ const createMatchMedia = (width: number) => (query: string) => ({
   dispatchEvent: jest.fn(),
 })
 
+const onEditMock = jest.fn()
+const onCopyMock = jest.fn()
+const onDeleteMock = jest.fn()
+
 function setup() {
   const user = userEvent.setup()
-  render(<Table {...Table.args} />)
+  render(
+    <Table
+      {...Table.args}
+      onCopyList={onCopyMock}
+      onEditList={onEditMock}
+      onDeleteList={onDeleteMock}
+    />
+  )
   return { user }
 }
 
 describe('[component] - ListTable', () => {
-  it('should render the list table', () => {
-    setup()
-    const table = screen.getByRole('table')
-    expect(table).toBeVisible()
-  })
-  it('should check for number of rows in list table and rows are visible', () => {
-    setup()
-    const tableRows = screen.getAllByRole('row')
-    if (Table.args?.rows) {
-      // adding one to length because using TableRow in the TableHead
-      expect(tableRows.length).toEqual(Table.args.rows.length + 1)
-    }
-    tableRows.forEach((row) => expect(row).toBeVisible())
-  })
-  it('should check for buttons in rows to be visible in desktop view ', () => {
+  it('should render component on desktop', () => {
     window.matchMedia = createMatchMedia(1024)
     setup()
+    const table = screen.getByRole('table')
+    const tableRows = screen.getAllByRole('row')
     const editBtns = screen.getAllByTestId('editBtn')
     const deleteBtns = screen.getAllByTestId('deleteBtn')
     const copyBtns = screen.getAllByTestId('copyBtn')
     const initiateQuoteBtns = screen.getAllByTestId('initiateQuoteBtn')
     const addToCartBtns = screen.getAllByTestId('addToCartBtn')
-    console.log(editBtns, deleteBtns, copyBtns, initiateQuoteBtns, addToCartBtns)
+    expect(table).toBeVisible()
+    tableRows.forEach((row) => expect(row).toBeVisible())
+    editBtns.forEach((btn) => expect(btn).toBeVisible())
+    deleteBtns.forEach((btn) => expect(btn).toBeVisible())
+    copyBtns.forEach((btn) => expect(btn).toBeVisible())
+    initiateQuoteBtns.forEach((btn) => expect(btn).toBeVisible())
+    addToCartBtns.forEach((btn) => expect(btn).toBeVisible())
   })
-  it('should check for menu button in rows to be visible in mobile view', () => {
+  it('should render component on mobile', () => {
     window.matchMedia = createMatchMedia(500)
     setup()
+    const table = screen.getByRole('table')
+    const tableRows = screen.getAllByRole('row')
     const menuBtn = screen.getAllByTestId('menuBtn')
-    console.log(menuBtn)
+    expect(table).toBeVisible()
+    tableRows.forEach((row) => expect(row).toBeVisible())
+    menuBtn.forEach((btn) => expect(btn).toBeVisible())
+  })
+  it('should call callback function when user clicks on Edit button', async () => {
+    window.matchMedia = createMatchMedia(1024)
+    const { user } = setup()
+    const editBtns = screen.getAllByTestId('editBtn')
+    editBtns.forEach((btn) => user.click(btn))
+    await waitFor(() => {
+      expect(onEditMock).toBeCalledTimes(editBtns.length)
+    })
+  })
+  it('should call callback function when user clicks on Delete button', async () => {
+    window.matchMedia = createMatchMedia(1024)
+    const { user } = setup()
+    const deleteBtns = screen.getAllByTestId('deleteBtn')
+    deleteBtns.forEach((btn) => user.click(btn))
+    await waitFor(() => {
+      expect(onDeleteMock).toBeCalledTimes(deleteBtns.length)
+    })
+  })
+  it('should call callback function when user clicks on Copy button', async () => {
+    window.matchMedia = createMatchMedia(1024)
+    const { user } = setup()
+    const copyBtns = screen.getAllByTestId('copyBtn')
+    copyBtns.forEach((btn) => user.click(btn))
+    await waitFor(() => {
+      expect(onCopyMock).toBeCalledTimes(copyBtns.length)
+    })
   })
 })
