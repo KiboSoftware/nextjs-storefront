de36e57c8cae1c70f97907557f02ac22822a8208
Krishna Mohan <krishna.m@ignitiv.com>
2023-07-31 23:59:25 +0530

    feature: CreateList Component

---
+++ components/my-account/Lists/CreateList/CreateList.style.tsx
@@ -0,0 +1,16 @@
+const style = {
+  input: {
+    maxWidth: '360px',
+    height: '32px',
+    fontSize: '14px',
+    padding: '8px 12px',
+    border: '1px solid #cdcdcd',
+    borderRadius: '4px',
+  },
+  listSection: {
+    display: 'flex',
+    fontSize: '12px',
+  },
+}
+
+export default style
