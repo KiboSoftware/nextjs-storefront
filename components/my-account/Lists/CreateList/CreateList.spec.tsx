4cdaa56c3be2a64956bfd8f5c7149adf9ae98376
Krishna Mohan <krishna.m@ignitiv.com>
2023-08-08 18:21:35 +0530

    add test cases for CreateList

--- components/my-account/Lists/CreateList/CreateList.spec.tsx
+++ components/my-account/Lists/CreateList/CreateList.spec.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 import { composeStories } from '@storybook/testing-react'
-import { render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import mediaQuery from 'css-mediaquery'
+import mockRouter from 'next-router-mock'
 
 import * as stories from './CreateList.stories'
 
@@ -20,9 +21,11 @@ const createMatchMedia = (width: number) => (query: string) => ({
   dispatchEvent: jest.fn(),
 })
 
+const onCreateFormToggleMock = jest.fn()
+
 function setup() {
   const user = userEvent.setup()
-  render(<Common {...Common.args} />)
+  render(<Common {...Common.args} onCreateFormToggle={onCreateFormToggleMock} />)
   return { user }
 }
 
@@ -43,6 +46,68 @@ describe('[componenet] - Create List', () => {
     user.type(listNameInput, newListName)
     await waitFor(() => {
       expect(listNameInput).toHaveValue(newListName)
+    })
+  })
+
+  it('should redirect to /my-account page in mobile view', async () => {
+    window.matchMedia = createMatchMedia(500)
+    const { user } = setup()
+    const myAccountBtn = screen.getByTestId('my-account-button')
+    expect(myAccountBtn).toBeVisible()
+    user.click(myAccountBtn)
+    await waitFor(() => {
+      expect(mockRouter).toMatchObject({
+        asPath: '/my-account',
+        pathname: '/my-account',
+      })
+    })
+  })
+
+  it('should redirect to /my-account page', async () => {
+    window.matchMedia = createMatchMedia(1024)
+    const { user } = setup()
+    const myAccountBtn = screen.getByTestId('my-account-button')
+    expect(myAccountBtn).toBeVisible()
+    user.click(myAccountBtn)
+    await waitFor(() => {
+      expect(mockRouter).toMatchObject({
+        asPath: '/my-account',
+        pathname: '/my-account',
+      })
+    })
+  })
+
+  it('should cancel create form', async () => {
+    window.matchMedia = createMatchMedia(1024)
+    const { user } = setup()
+    const cancelBtn = screen.getByText(/cancel/i)
+    expect(cancelBtn).toBeVisible()
+
+    user.click(cancelBtn)
+
+    await waitFor(() => {
+      expect(onCreateFormToggleMock).toBeCalled()
+    })
+  })
+
+  it.only('should save and close create form', async () => {
+    window.matchMedia = createMatchMedia(1024)
+    setup()
+    const saveAndCloseBtn = screen.getByText(/save-and-close/i)
+    expect(saveAndCloseBtn).toBeVisible()
+    expect(saveAndCloseBtn).toBeDisabled()
+
+    const listNameInput = screen.getByPlaceholderText(/name-this-list/i)
+
+    fireEvent.change(listNameInput, { target: { value: 'New List' } })
+
+    expect(listNameInput).toHaveValue('New List')
+    expect(saveAndCloseBtn).toBeEnabled()
+
+    fireEvent.click(saveAndCloseBtn)
+
+    await waitFor(() => {
+      expect(onCreateFormToggleMock).toBeCalled()
     })
   })
 })
