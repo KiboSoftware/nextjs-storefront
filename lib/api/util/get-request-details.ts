09559940f4ad3d7f05d2ee817f438fcf02fd0160
kibo-sushant <sushant.jadhav@kibocommerce.com>
2024-01-02 17:57:20 +0530

    feat: reading userId from cookie

--- lib/api/util/get-request-details.ts
+++ lib/api/util/get-request-details.ts
@@ -1,28 +1,44 @@
 import { NextApiRequest } from 'next'
 import getConfig from 'next/config'
 
-const getUserId = (req: NextApiRequest) => {
+const getDecodeCookie = (req: NextApiRequest) => {
   const config = getConfig()
   const cookieName = config?.publicRuntimeConfig.userCookieKey.toLowerCase()
 
   const cookie = req.cookies?.[cookieName]?.split(';')[0] || ''
   const decodedCookie = JSON.parse(Buffer.from(cookie, 'base64').toString('ascii'))
 
-  return decodedCookie?.userId
+  return decodedCookie
 }
 
-const getTenant = () => {
-  const KIBO_API_HOST = process.env.KIBO_API_HOST || ''
-  const tenantArr = KIBO_API_HOST.match(/t(\d+)-/)
-  const tenant = tenantArr && tenantArr[1]
+const getUserId = (req: NextApiRequest) => {
+  return getDecodeCookie(req)?.userId
+}
+
+const getTenant = (req: NextApiRequest) => {
+  const KIBO_API_HOST = process.env.KIBO_API_HOST
+
+  let tenant
+  if (KIBO_API_HOST) {
+    const tenantArr = KIBO_API_HOST.match(/t(\d+)-/)
+    tenant = tenantArr && tenantArr[1]
+  } else {
+    tenant = getDecodeCookie(req)?.tenant
+  }
 
   return tenant || ''
 }
 
-const getSite = () => {
-  const KIBO_API_HOST = process.env.KIBO_API_HOST || ''
-  const siteArr = KIBO_API_HOST.match(/s(\d+)\./)
-  const site = siteArr && siteArr[1]
+const getSite = (req: NextApiRequest) => {
+  const KIBO_API_HOST = process.env.KIBO_API_HOST
+
+  let site
+  if (KIBO_API_HOST) {
+    const siteArr = KIBO_API_HOST.match(/s(\d+)\./)
+    site = siteArr && siteArr[1]
+  } else {
+    site = getDecodeCookie(req)?.site
+  }
 
   return site || ''
 }
@@ -33,8 +49,8 @@ const getRequestDetails = (req: NextApiRequest) => {
     apiEndpoint: req.url,
     // query: req.body.query,
     // variables: req.body.variables,
-    tenant: getTenant(),
-    site: getSite(),
+    tenant: getTenant(req),
+    site: getSite(req),
     userId: getUserId(req),
     'X-Forwarded-For': req.socket.remoteAddress,
     'ip-address': req.socket.remoteAddress,
