a836e15703efaf1e6d81000b2c71b3be7cf4eb31
kibo-sushant <sushant.jadhav@kibocommerce.com>
2024-01-04 22:06:09 +0530

    feat: removed unwanted console

--- lib/api/handlers/logger.ts
+++ lib/api/handlers/logger.ts
@@ -4,7 +4,6 @@ import type { NextApiResponse } from 'next'
 
 export default async function loggerHandler(req: NextApiRequestWithLogger, res: NextApiResponse) {
   try {
-    console.log('------------req.body-----------', req.body)
     req.logger.error(req.body, 'Client Error')
   } catch (error: any) {
     res.status(error?.code).json({ message: error?.message })
