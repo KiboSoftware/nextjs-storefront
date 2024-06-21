112151c1f5ec35821a8537e6a358bbe714852921
kibo-snehanerekar <sneha.nerekar@kibocommerce.com>
2024-06-21 11:42:49 +0530

    ICKY 1091 Review comments and demo Feedback

--- components/layout/AppHeader/Icons/AccountIcon/AccountIcon.tsx
+++ components/layout/AppHeader/Icons/AccountIcon/AccountIcon.tsx
@@ -15,14 +15,26 @@ const AccountIcon = ({ size, isElementVisible, isCSR, onAccountIconClick }: Acco
   const { t } = useTranslation('common')
 
   return (
-    <HeaderAction
-      title={(isAuthenticated || isCSR) && user?.firstName ? `${t('hi')}, ${user?.firstName}` : ''}
-      subtitle={isAuthenticated || isCSR ? t('go-to-my-account') : t('log-in')}
-      icon={AccountCircleIcon}
-      iconFontSize={size}
-      isElementVisible={isElementVisible}
-      onClick={onAccountIconClick}
-    />
+    <>
+      {!isCSR && (
+        <HeaderAction
+          title={isAuthenticated && user?.firstName ? `${t('hi')}, ${user?.firstName}` : ''}
+          subtitle={isAuthenticated ? t('go-to-my-account') : t('log-in')}
+          icon={AccountCircleIcon}
+          iconFontSize={size}
+          isElementVisible={isElementVisible}
+          onClick={onAccountIconClick}
+        />
+      )}
+      {isCSR && (
+        <HeaderAction
+          title={user?.firstName ? `${t('hi')}, ${user?.firstName}` : ''}
+          icon={AccountCircleIcon}
+          iconFontSize={size}
+          isElementVisible={isElementVisible}
+        />
+      )}
+    </>
   )
 }
 
