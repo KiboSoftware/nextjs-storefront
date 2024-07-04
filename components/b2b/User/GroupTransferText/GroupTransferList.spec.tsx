42c8e2528f1847fe6955028534a94bf3a237e732
Kibo-TriptiV <122886510+Kibo-TriptiV@users.noreply.github.com>
2024-07-04 19:17:18 +0530

    icky-1140 written test cases for selected grp

--- components/b2b/User/GroupTransferText/GroupTransferList.spec.tsx
+++ components/b2b/User/GroupTransferText/GroupTransferList.spec.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from '@storybook/testing-react'
-import { render, fireEvent, screen, waitFor } from '@testing-library/react'
+import { render, within, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
@@ -31,24 +31,49 @@ describe('[component] Group Transfer List', () => {
     expect(screen.getByText('Admin')).toBeInTheDocument()
   })
 
-  it('moves all items to right when move all right button is clicked', () => {
+  it('moves all items to right when move all right button is clicked', async () => {
     const { user } = setup()
 
     const moveAllRightButton = screen.getByRole('button', { name: 'move all right' })
-    user.click(moveAllRightButton)
+    await user.click(moveAllRightButton)
 
-    const rightListItems = screen.getAllByRole('listitem')
-    expect(rightListItems).toHaveLength(3)
+    const rightGroupContainer = screen.getByTestId('selectedGroups')
+    const rightListItems = within(rightGroupContainer).getAllByRole('listitem')
+    await waitFor(() => {
+      expect(rightListItems).toHaveLength(3)
+    })
   })
 
-  it('moves all items to left when move all left button is clicked', () => {
+  it('moves all items to left when move all left button is clicked', async () => {
     const { user } = setup()
 
     const moveAllLeftButton = screen.getByRole('button', { name: 'move all left' })
-    user.click(moveAllLeftButton)
+    await user.click(moveAllLeftButton)
 
-    const leftListItems = screen.getAllByRole('listitem')
-    expect(leftListItems).toHaveLength(3)
+    const leftGroupContainer = screen.getByTestId('allGroups')
+    const leftListItems = within(leftGroupContainer).getAllByRole('listitem')
+    await waitFor(() => {
+      expect(leftListItems).toHaveLength(3)
+    })
+  })
+
+  it('moves checked items to left when move selected left button is clicked', async () => {
+    const { user } = setup()
+
+    const group1Item = screen.getByText('Employee')
+    await user.click(group1Item)
+
+    const moveSelectedLeftButton = screen.getByRole('button', { name: 'move selected left' })
+    await user.click(moveSelectedLeftButton)
+
+    const moveSelectedRightButton = screen.getByRole('button', { name: 'move selected right' })
+    expect(moveSelectedRightButton).toBeDisabled()
+
+    const leftGroupContainer = screen.getByTestId('allGroups')
+    const leftListItems = within(leftGroupContainer).getAllByRole('listitem')
+    await waitFor(() => {
+      expect(leftListItems).toHaveLength(3)
+    })
   })
 
   it('moves checked items to right when move selected right button is clicked', async () => {
@@ -56,8 +81,6 @@ describe('[component] Group Transfer List', () => {
 
     const group1Item = screen.getByText('Manager')
     await user.click(group1Item)
-    const group2Item = screen.getByText('Admin')
-    await user.click(group2Item)
 
     const moveSelectedRightButton = screen.getByRole('button', { name: 'move selected right' })
     await user.click(moveSelectedRightButton)
@@ -65,8 +88,11 @@ describe('[component] Group Transfer List', () => {
     const moveSelectedLeftButton = screen.getByRole('button', { name: 'move selected left' })
     expect(moveSelectedLeftButton).toBeDisabled()
 
-    const rightListItems = screen.getAllByRole('listitem')
-    expect(rightListItems).toHaveLength(2)
+    const rightGroupContainer = screen.getByTestId('selectedGroups')
+    const rightListItems = within(rightGroupContainer).getAllByRole('listitem')
+    await waitFor(() => {
+      expect(rightListItems).toHaveLength(2)
+    })
   })
 
   it('submits added and removed groups correctly when submitting', async () => {
@@ -78,9 +104,11 @@ describe('[component] Group Transfer List', () => {
     await user.click(moveSelectedRightButton)
 
     const addGroups = [{ accountId: 1100, code: 'admin', name: 'Admin', description: 'admin' }]
-    expect(mockOnAddRemoveGroups).toHaveBeenCalledWith({
-      addGroups: addGroups,
-      removeGroups: [],
+    await waitFor(() => {
+      expect(mockOnAddRemoveGroups).toHaveBeenCalledWith({
+        addGroups: addGroups,
+        removeGroups: [],
+      })
     })
   })
 })
