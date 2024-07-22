5a6229a206da4248659065e418bd13747bdbbe1f
kibo-sushant <sushant.jadhav@kibocommerce.com>
2024-07-22 14:55:11 +0530

    feat: fixed failing testcase

--- lib/api/__test__/util/get-paypal-bearer-token.spec.ts
+++ lib/api/__test__/util/get-paypal-bearer-token.spec.ts
@@ -1,29 +1,15 @@
 import getPayPalBearerToken from '../../util/get-paypal-bearer-token'
-import { paypalCheckoutSettingsMock } from '@/__mocks__/stories'
 
 const mockFetch = jest.fn()
 global.fetch = mockFetch // Assign the mock to global fetch
 
-const firstMockResponse = paypalCheckoutSettingsMock // First mock response
-const secondMockResponse = { access_token: 'bearerToken' }
+const firstMockResponse = { access_token: 'bearerToken' }
 
 // Mock the first call to fetch
 mockFetch.mockResolvedValueOnce({
   json: () => firstMockResponse,
   ok: true,
 })
-
-// Mock the second call to fetch
-mockFetch.mockResolvedValueOnce({
-  json: () => secondMockResponse,
-  ok: true,
-})
-
-jest.mock('@/lib/api/util/api-auth-client', () => ({
-  apiAuthClient: {
-    getAccessToken: () => Promise.resolve('bearerToken'),
-  },
-}))
 
 describe('getPayPalBearerToken util function', () => {
   it('should PayPal Bearer Token', async () => {
