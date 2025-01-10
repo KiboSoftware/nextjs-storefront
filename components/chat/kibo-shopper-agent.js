cb4435e97fb4490ec21fa5ab08c520a475524de8
kibo-kevinwatts <kevin.watts@kibocommerce.com>
2025-01-10 11:08:07 -0600

    fix: wip

--- components/chat/kibo-shopper-agent.js
+++ components/chat/kibo-shopper-agent.js
@@ -129,13 +129,17 @@ class KiboShopperAgent {
     }
   }
   init() {
-    if(!this.dfMessenger){
+    setTimeout(() => {
+
+    if(!this.dfMessenger) {
       document.body.insertAdjacentHTML('beforeend', `<df-messenger chat-title="KiboShopper" location="us" project-id="kibo-bq-dev-presentation" agent-id="33c36d2a-8171-4f3a-807e-50bafe98c3c1" max-query-length="-1" language-code="en"><df-messenger-chat-bubble chat-title="Shopping Agent"></df-messenger-chat-bubble></df-messenger>`)
     }
     setTimeout(() => {
       this.defineCustomElements()
       this.registerChatTools()  
-    }, 100)
+    }, 500)
+  }, 1000)
+
   }
 }
 // customElements.define('kibo-shopper-agent', KiboShopperAgent);
