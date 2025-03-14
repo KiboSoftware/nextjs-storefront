6a2e9b9d3eedb5a994eef332bd115c3ee8216004
kibo-kevinwatts <kevin.watts@kibocommerce.com>
2025-03-14 18:33:46 -0500

    fix: include ability to reset chat convo

--- components/core/ChatwootWidget/ChatwootWidget.tsx
+++ components/core/ChatwootWidget/ChatwootWidget.tsx
@@ -10,6 +10,13 @@ declare global {
     $chatwoot: any
   }
 }
+const resetChatwootButton = () => {
+  const resetButton = document.createElement('button')
+  resetButton.className = 'woot-widget-bubble woot-elements--left'
+  resetButton.setAttribute('style', 'background: rgb(0, 156, 224);')
+  resetButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="50px" height="50px" fill-rule="nonzero"><g fill="#eff0f4" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.12,5.12)"><path d="M25,2c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175c10.51712,0 19,8.48288 19,19c0,10.51712 -8.48288,19 -19,19c-10.51712,0 -19,-8.48288 -19,-19c0,-5.4758 2.30802,-10.39189 6,-13.85547v3.85547c-0.0102,0.72127 0.36875,1.39216 0.99175,1.75578c0.623,0.36361 1.39351,0.36361 2.01651,0c0.623,-0.36361 1.00195,-1.0345 0.99175,-1.75578v-11h-11c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h4.52539c-4.61869,4.20948 -7.52539,10.27232 -7.52539,17c0,12.67888 10.32112,23 23,23c12.67888,0 23,-10.32112 23,-23c0,-12.67888 -10.32112,-23 -23,-23z"></path></g></g></svg>`
+  return resetButton
+}
 
 const ChatwootWidget = () => {
   const { isAuthenticated, user } = useAuthContext()
@@ -41,7 +48,12 @@ const ChatwootWidget = () => {
         })
       }
     }
-
+    const resetChatwoot = () => {
+      if (window.$chatwoot && user) {
+        window.$chatwoot.reset()
+        window.$chatwoot.setUser(user.userId, { email: user.emailAddress })
+      }
+    }
     if (!isAuthenticated) {
       if (window.chatwootSDK) {
         window.$chatwoot.toggleBubbleVisibility('hide')
@@ -68,6 +80,13 @@ const ChatwootWidget = () => {
             if (window.$chatwoot && user) {
               try {
                 window.$chatwoot.setUser(user.userId, { email: user.emailAddress })
+                const bubbleHolder = document.getElementById('cw-bubble-holder')
+                const resetButton = resetChatwootButton()
+                resetButton.addEventListener('click', (e) => {
+                  e.stopPropagation()
+                  resetChatwoot()
+                })
+                bubbleHolder?.appendChild(resetButton)
               } catch (error) {
                 console.error('Error setting Chatwoot user:', error)
               }
