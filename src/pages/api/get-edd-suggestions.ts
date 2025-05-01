ce59cd7ad436bf2fbdb291c921e5e990708248d6
kibo-chandradeeptalaha <89371824+kibo-chandradeeptalaha@users.noreply.github.com>
2025-05-01 19:35:12 +0530

    ICKY-1579: added api handler

--- src/pages/api/get-edd-suggestions.ts
+++ src/pages/api/get-edd-suggestions.ts
@@ -1,36 +1,4 @@
-import { apiAuthClient } from '@/lib/api/util/api-auth-client'
-import { NextApiRequestWithLogger } from '@/lib/types'
+import { getEddSuggestionsHandler } from '@/lib/api/handlers'
+import withLogger from '@/lib/api/util/with-logger'
 
-import type { NextApiResponse } from 'next'
-
-export default async function getEDDSuggestions(
-  req: NextApiRequestWithLogger,
-  res: NextApiResponse
-) {
-  try {
-    const authToken = await apiAuthClient.getAccessToken()
-    const apiHost = process.env.KIBO_API_HOST as string
-    const tenantAndSite = apiHost.split('.')[0]
-    const tenantId = tenantAndSite.split('-')[0].split('t')[1].toString()
-    const siteId = tenantAndSite.split('-')[1].split('s')[1].toString()
-
-    const url = `${process.env.KIBO_BASE_PATH}/kibo.orderrouting.webapi/commerce/orders/orderRouting/api/v1/routing/edd/suggestion?returnSuggestionLog=true`
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-        'Content-Type': 'application/json',
-        'x-vol-tenant': tenantId,
-        'x-vol-site': siteId,
-      },
-      body: req.body,
-    })
-
-    const eddSuggestionData = await response.json()
-
-    return res.status(200).json(eddSuggestionData || {})
-  } catch (e) {
-    console.error(e)
-    return res.status(200).json({})
-  }
-}
+export default withLogger(getEddSuggestionsHandler as any)
