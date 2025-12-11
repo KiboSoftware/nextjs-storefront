d40093052ec90f73e1baf9396c7de944d28b8cb7
kibo-geetanshuchhabra <geetanshu.chhabra@kibocommerce.com>
2025-12-11 22:49:28 +0530

    revert update account hierarchy behavior for purchaser system role

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -281,7 +281,7 @@ export const SystemRoleBehaviors: Record<string, Record<number, number[]>> = {
     2005: [2015, 2014, 2016],
     2006: [2021, 2032, 2020, 2022],
     2007: [2023],
-    2008: [2024],
+    2008: [],
     2009: [2031],
     2010: [2029],
   },
