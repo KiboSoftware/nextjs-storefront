1a5c95e567c6401e21683442e7655f340ec33821
kibo-geetanshuchhabra <geetanshu.chhabra@kibocommerce.com>
2025-12-18 23:28:30 +0530

    fix search term issue while navigate to other page using links

--- components/layout/SearchSuggestions/SearchSuggestions.tsx
+++ components/layout/SearchSuggestions/SearchSuggestions.tsx
@@ -116,6 +116,19 @@ const SearchSuggestions = (props: SearchSuggestionsProps) => {
     searchTerm.trim() ? handleOpen() : handleClose()
   }, [searchTerm])
 
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setSearchTerm('')
+      handleClose()
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Stack width="100%" position="relative" gap={1} sx={{ maxWidth: { xs: '100%', md: '65%' } }}>
       <Box sx={{ zIndex: 1400 }}>
