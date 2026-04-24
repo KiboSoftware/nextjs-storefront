f685fb3cb32a15ad175a1c6d2e40c6c3ac0d697f
kibo-kevinwatts <kevin.watts@kibocommerce.com>
2026-04-24 10:36:54 -0500

    enable multi-ship

--- next.config.js
+++ next.config.js
@@ -124,7 +124,7 @@ module.exports = {
     defaultWishlistName: DEFAULT_WISHLIST_NAME,
     pciHost: process.env.KIBO_PCI_HOST,
     apiHost: process.env.KIBO_API_HOST,
-    isMultiShipEnabled: false,
+    isMultiShipEnabled: true,
     shipOptions: [
       {
         value: 'ShipToHome',
@@ -191,7 +191,7 @@ module.exports = {
     pageSize: 1000,
     cacheKey: 'categoryTree',
     cacheTimeOut: 10000,
-    isMultiShipEnabled: false,
+    isMultiShipEnabled: true,
     pageConfig: {
       productDetail: {
         staticPathsMaxSize: 10,
